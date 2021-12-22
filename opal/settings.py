e745b24d60213dd2f11fb0a6fd72ecfe3ea8b93e
Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>
2021-12-22 12:04:16 -0500

    Reorder middlewares for proper locale support (#11).

--- opal/settings.py
+++ opal/settings.py
@@ -62,8 +62,8 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
