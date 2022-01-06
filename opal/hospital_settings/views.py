3ad0de441e3fc1dc72e79724853de75b394eea93
Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>
2022-01-06 10:15:06 -0500

    Adds ability to filter using query parameters using django-filter (#9).

--- opal/hospital_settings/views.py
+++ opal/hospital_settings/views.py
@@ -8,9 +8,15 @@ class InstitutionViewSet(viewsets.ModelViewSet):
     queryset = Institution.objects.all()
     serializer_class = InstitutionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filterset_fields = ['code']
 
 
 class SiteViewSet(viewsets.ModelViewSet):
     queryset = Site.objects.all()
     serializer_class = SiteSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # see: https://github.com/carltongibson/django-filter/issues/1076#issuecomment-489252242
+    filterset_fields = {
+        'code': ['in'],
+        'institution__code': ['exact'],
+    }
