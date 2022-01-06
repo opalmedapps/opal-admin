4566915b563109548f8daf78e40883c0896fb01e
Matthias Schoettle <git@mattsch.com>
2022-01-06 10:15:06 -0500

    Adds ability to filter using query parameters using django-filter (#9).

--- opal/settings.py
+++ opal/settings.py
@@ -50,6 +50,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     'django_extensions',
     'rest_framework',
+    'django_filters',
 ]
 
 LOCAL_APPS = [
@@ -267,4 +268,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
