135cd0cb165d1f361e7233c73959271f8b86e23b
Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>
2022-01-06 11:48:08 -0500

    Improves hospital settings models
    Moves parking_url to Site and makes code unique.
    Adds more tests.

--- opal/hospital_settings/tests/test_models.py
+++ opal/hospital_settings/tests/test_models.py
@@ -1,4 +1,6 @@
-from ..models import Institution
+import pytest
+
+from ..models import Institution, Site
 
 
 def test_location_string():
@@ -6,3 +8,25 @@ def test_location_string():
     institution = Institution(name='Test Hospital', code='TH')
 
     assert str(institution) == 'Test Hospital'
+
+
+@pytest.mark.django_db
+def test_institution_ordered():
+    Institution.objects.create(name='Test Hospital', code='TH')
+    Institution.objects.create(name='ATest Hospital', code='TH2')
+
+    first = Institution.objects.all()[0]
+
+    assert first.name == 'ATest Hospital'
+
+
+@pytest.mark.django_db
+def test_site_ordered():
+    institution = Institution.objects.create(name='Test Hospital', code='TH')
+
+    Site.objects.create(name='Test Site', code='TST', institution=institution)
+    Site.objects.create(name='ATest Site', code='TST2', institution=institution)
+
+    first = Site.objects.all()[0]
+
+    assert first.name == 'ATest Site'
--- opal/hospital_settings/translation.py
+++ opal/hospital_settings/translation.py
@@ -3,6 +3,11 @@ from modeltranslation.translator import TranslationOptions, register
 from .models import Institution, Site
 
 
-@register([Institution, Site])
-class LocationTranslationOptions(TranslationOptions):
-    fields = ('name', 'parking_url')
+@register(Institution)
+class InstitutionTranslationOptions(TranslationOptions):
+    fields = ('name', )
+
+
+@register(Site)
+class SiteTranslationOptions(TranslationOptions):
+    fields = ('name', 'parking_url', )
