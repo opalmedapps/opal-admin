51b3f1160345563a86dee2689aafd123615393eb
Matthias Schoettle <git@mattsch.com>
2022-01-06 15:22:09 -0500

    Improve API and add integration tests

--- opal/hospital_settings/urls.py
+++ opal/hospital_settings/urls.py
@@ -5,6 +5,8 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
+# add trailing_slash=False if the trailing slash should not be enforced
+# see: https://www.django-rest-framework.org/api-guide/routers/#defaultrouter
 router = DefaultRouter()
 router.register(r'institutions', views.InstitutionViewSet, basename='institution')
 router.register(r'sites', views.SiteViewSet, basename='site')
