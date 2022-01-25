a26add444a7c942c9cb44af569c18c9aa7543fe0
Anton Gladyr <anton.gladyr@gmail.com>
2022-01-25 13:26:37 -0500

    Add initial custom admin templates using generic views and crispy

--- opal/hospital_settings/urls.py
+++ opal/hospital_settings/urls.py
@@ -11,6 +11,32 @@ router = DefaultRouter()
 router.register(r'institutions', views.InstitutionViewSet, basename='institution')
 router.register(r'sites', views.SiteViewSet, basename='site')
 
+
 urlpatterns = [
-    path('', include(router.urls)),
+    # REST APIs
+    path('api/hospital-settings/', include(router.urls)),
+
+    # Web pages
+    path('', views.HomePageView.as_view(), name='index'),
+    path('hospital-settings/institutions/', views.InstitutionListView.as_view(), name='institution-list'),
+    path(
+        'hospital-settings/institution/<int:pk>/',
+        views.InstitutionDetailView.as_view(),
+        name='institution-detail'
+    ),
+    path(
+        'hospital-settings/institution/create/',
+        views.InstitutionCreateView.as_view(),
+        name='institution-create'
+    ),
+    path(
+        'hospital-settings/institution/<int:pk>/update/',
+        views.InstitutionUpdateView.as_view(),
+        name='institution-update'
+    ),
+    path(
+        'hospital-settings/institution/<int:pk>/delete/',
+        views.InstitutionDeleteView.as_view(),
+        name='institution-delete'
+    ),
 ]
--- opal/hospital_settings/views.py
+++ opal/hospital_settings/views.py
@@ -1,9 +1,16 @@
+from django.urls import reverse_lazy
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+
 from rest_framework import permissions, viewsets
 
 from .models import Institution, Site
 from .serializers import InstitutionSerializer, SiteSerializer
 
 
+# REST API
 class InstitutionViewSet(viewsets.ModelViewSet):
     queryset = Institution.objects.all()
     serializer_class = InstitutionSerializer
@@ -20,3 +27,54 @@ class SiteViewSet(viewsets.ModelViewSet):
         'code': ['in'],
         'institution__code': ['exact'],
     }
+
+
+# Web pages
+class HomePageView(TemplateView):
+    template_name = 'hospital_settings/index.html'
+
+
+# INSTITUTIONS
+class InstitutionListView(ListView):
+    model = Institution
+    template_name = 'hospital_settings/institution/institution_list.html'
+
+
+class InstitutionCreateView(CreateView):
+    model = Institution
+    template_name = 'hospital_settings/institution/institution_form.html'
+    fields = ['name_en', 'name_fr', 'code']
+    success_url = reverse_lazy('institution-list')
+
+
+class InstitutionDetailView(DetailView):
+    model = Institution
+    template_name = 'hospital_settings/institution/institution_detail.html'
+
+
+class InstitutionUpdateView(UpdateView):
+    model = Institution
+    template_name = 'hospital_settings/institution/institution_form.html'
+    fields = fields = ['name_en', 'name_fr', 'code']
+    success_url = reverse_lazy('institution-list')
+
+
+class InstitutionDeleteView(DeleteView):
+    model = Institution
+    success_url = reverse_lazy('institution-list')
+
+
+# SITES
+class SiteCreateView(CreateView):
+    model = Site
+    fields = ['name_en', 'name_fr', 'parking_url_en', 'parking_url_fr', 'code', 'institution']
+
+
+class SiteUpdateView(UpdateView):
+    model = Site
+    fields = ['name_en', 'name_fr', 'parking_url_en', 'parking_url_fr', 'code', 'institution']
+
+
+class SiteDeleteView(DeleteView):
+    model = Site
+    success_url = reverse_lazy('hospital-settings')
