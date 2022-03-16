7767e42ee308177bc815fdd07155a18f1289ec8f
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2022-03-16 09:44:44 -0400

    Update models and views unit tests

--- opal/hospital_settings/tests/test_models.py
+++ opal/hospital_settings/tests/test_models.py
@@ -6,12 +6,13 @@ TEST_HOSPITAL = 'Test Hospital'
 
 pytestmark = pytest.mark.django_db
 
+# Institution tests
 
-def test_location_string() -> None:
-    """This test ensures the __str__ method is defined."""
+
+def test_institution_string_method() -> None:
+    """This test ensures the `__str__` method is defined for the `Institution` model."""
     # Location is abstract and cannot be instantiated directly
     institution = Institution(name=TEST_HOSPITAL, code='TH')
-
     assert str(institution) == TEST_HOSPITAL
 
 
@@ -19,19 +20,101 @@ def test_institution_ordered() -> None:
     """This test ensures that the institutions are ordered by name."""
     Institution.objects.create(name=TEST_HOSPITAL, code='TH')
     Institution.objects.create(name='ATest Hospital', code='TH2')
-
     first = Institution.objects.all()[0]
-
     assert first.name == 'ATest Hospital'
+
+
+def test_institution_name_label(institution: Institution) -> None:
+    """This test checks that the `name` field label (`verbose_name`) is as expected."""
+    field_label = institution._meta.get_field('name').verbose_name
+    assert field_label == 'Name'
+
+
+def test_institution_name_max_length(institution: Institution) -> None:
+    """This test checks that the size of the `name` field is as expected."""
+    max_length = institution._meta.get_field('name').max_length
+    assert max_length == 100
+
+
+def test_institution_code_label(institution: Institution) -> None:
+    """This test checks that the `code` field label (`verbose_name`) is as expected."""
+    field_label = institution._meta.get_field('code').verbose_name
+    assert field_label == 'Code'
+
+
+def test_institution_code_max_length(institution: Institution) -> None:
+    """This test checks that the size of the `code` field is as expected."""
+    max_length = institution._meta.get_field('code').max_length
+    assert max_length == 10
+
+
+def test_institution_verbose_name(institution: Institution) -> None:
+    """This test checks that `Institution` verbose name is as expected."""
+    verbose_name = institution._meta.verbose_name
+    assert verbose_name == 'Institution'
+
+
+def test_institution_verbose_name_plural(institution: Institution) -> None:
+    """This test checks that `Institution` verbose plural name is as expected."""
+    verbose_name_plural = institution._meta.verbose_name_plural
+    assert verbose_name_plural == 'Institutions'
+
+# Site tests
+
+
+def test_site_name_label(site: Site) -> None:
+    """This test checks that the `name` field label (`verbose_name`) is as expected."""
+    field_label = site._meta.get_field('name').verbose_name
+    assert field_label == 'Name'
+
+
+def test_site_name_max_length(site: Site) -> None:
+    """This test checks that the size of the `name` field is as expected."""
+    max_length = site._meta.get_field('name').max_length
+    assert max_length == 100
+
+
+def test_site_code_label(site: Site) -> None:
+    """This test checks that the `code` field label (`verbose_name`) is as expected."""
+    field_label = site._meta.get_field('code').verbose_name
+    assert field_label == 'Code'
+
+
+def test_site_code_max_length(site: Site) -> None:
+    """This test checks that the size of the `code` field is as expected."""
+    max_length = site._meta.get_field('code').max_length
+    assert max_length == 10
+
+
+def test_site_verbose_name(site: Site) -> None:
+    """This test checks that `Site` verbose name is as expected."""
+    verbose_name = site._meta.verbose_name
+    assert verbose_name == 'Site'
+
+
+def test_site_verbose_name_plural(site: Site) -> None:
+    """This test checks that `Site` verbose plural name is as expected."""
+    verbose_name_plural = site._meta.verbose_name_plural
+    assert verbose_name_plural == 'Sites'
+
+
+def test_site_parking_url_label(site: Site) -> None:
+    """This test checks that the `parking_url` field label (`verbose_name`) is as expected."""
+    field_label = site._meta.get_field('parking_url').verbose_name
+    assert field_label == 'Parking Info'
+
+
+def test_site_string_method(institution: Institution) -> None:
+    """This test ensures the `__str__` method is defined for the `Site` model."""
+    # Location is abstract and cannot be instantiated directly
+    site = Site(name=TEST_HOSPITAL, code='TH', institution=institution)
+    assert str(site) == TEST_HOSPITAL
 
 
 def test_site_ordered() -> None:
     """This test ensures that the sites are ordered by name."""
     institution = Institution.objects.create(name=TEST_HOSPITAL, code='TH')
-
     Site.objects.create(name='Test Site', code='TST', institution=institution)
     Site.objects.create(name='ATest Site', code='TST2', institution=institution)
-
     first = Site.objects.all()[0]
-
     assert first.name == 'ATest Site'
--- opal/hospital_settings/tests/test_views.py
+++ opal/hospital_settings/tests/test_views.py
@@ -5,7 +5,8 @@ from django.test import Client
 from django.urls.base import reverse
 
 import pytest
-from pytest_django.asserts import assertRedirects, assertTemplateUsed
+from bs4 import BeautifulSoup
+from pytest_django.asserts import assertContains, assertRedirects, assertTemplateUsed
 
 from ..models import Institution, Site
 
@@ -74,6 +75,29 @@ def test_hs_institution_uses_correct_template(
     assertTemplateUsed(response, template)
 
 
+def test_institution_list_dislplays_all(client: Client):
+    """This test ensures that the institution list page template displays all the institutions."""
+    Institution.objects.bulk_create([
+        Institution(name_en='TEST1_EN', name_fr='TEST1_FR', code='TEST1'),
+        Institution(name_en='TEST2_EN', name_fr='TEST2_FR', code='TEST2'),
+        Institution(name_en='TEST3_EN', name_fr='TEST3_FR', code='TEST3'),
+    ])
+    url = reverse('hospital-settings:institution-list')
+    response = client.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    returned_institutions = soup.find('tbody').find_all('tr')
+    assert len(returned_institutions) == Institution.objects.count()
+
+
+def test_institution_update_object_displayed(client: Client, institution: Institution):
+    """This test ensures that the institution detail page displays all fields."""
+    url = reverse('hospital-settings:institution-update', args=(institution.id,))
+    response = client.get(url)
+    assertContains(response, 'TEST1_EN')
+    assertContains(response, 'TEST1_FR')
+    assertContains(response, 'TEST1')
+
+
 # SITE
 
 # tuple with `Site` templates and corresponding url names
@@ -108,6 +132,61 @@ def test_hs_site_uses_correct_template(
     url = reverse(url_name, args=(site.id,))
     response = client.get(url)
     assertTemplateUsed(response, template)
+
+
+def test_list_all_sites(client: Client, institution: Institution):
+    """This test ensures that the site list page template displays all the institutions."""
+    Site.objects.bulk_create([
+        Site(
+            name_en='TEST1_EN',
+            name_fr='TEST1_FR',
+            parking_url_en='http://127.0.0.1:8000/hospital-settings/site/1/',
+            parking_url_fr='http://127.0.0.1:8000/hospital-settings/site/1/',
+            code='TEST1',
+            institution=institution,
+        ),
+        Site(
+            name_en='TEST2_EN',
+            name_fr='TEST2_FR',
+            parking_url_en='http://127.0.0.1:8000/hospital-settings/site/2/',
+            parking_url_fr='http://127.0.0.1:8000/hospital-settings/site/2/',
+            code='TEST2',
+            institution=institution,
+        ),
+        Site(
+            name_en='TEST3_EN',
+            name_fr='TEST3_FR',
+            parking_url_en='http://127.0.0.1:8000/hospital-settings/site/3/',
+            parking_url_fr='http://127.0.0.1:8000/hospital-settings/site/3/',
+            code='TEST3',
+            institution=institution,
+        ),
+    ])
+    url = reverse('hospital-settings:site-list')
+    response = client.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    returned_sites = soup.find('tbody').find_all('tr')
+    assert len(returned_sites) == Site.objects.count()
+
+
+def test_site_update_object_displayed(client: Client, institution: Institution):
+    """This test ensures that the site detail page displays all the fields."""
+    site = Site.objects.create(
+        name_en='TEST1_EN',
+        name_fr='TEST1_FR',
+        parking_url_en='http://127.0.0.1:8000/hospital-settings/site/1/fr',
+        parking_url_fr='http://127.0.0.1:8000/hospital-settings/site/1/en',
+        code='TEST1',
+        institution=institution,
+    )
+    url = reverse('hospital-settings:site-update', args=(site.id,))
+    response = client.get(url)
+    assertContains(response, 'TEST1_EN')
+    assertContains(response, 'TEST1_FR')
+    assertContains(response, 'http://127.0.0.1:8000/hospital-settings/site/1/fr')
+    assertContains(response, 'http://127.0.0.1:8000/hospital-settings/site/1/en')
+    assertContains(response, 'TEST1')
+    assertContains(response, institution.name)
 
 
 # SUCCESSFUL REDIRECTS
