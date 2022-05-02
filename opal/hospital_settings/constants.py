f011373c8c575899ad89b4c38be5fc4daedf72cd
Dominic Bourdua <dominic.bourdua@gmail.com>
2022-05-02 17:59:40 +0000

    feat: Add user patient relationship type model
    See merge request opalmedapps/backend!84

---
+++ opal/hospital_settings/constants.py
@@ -0,0 +1,7 @@
+"""List of constants for Hospital Settings."""
+from typing import Final
+
+#: Maximum possible patient age for a relationship.
+RELATIONSHIP_MAX_AGE: Final = 150
+#: Minimum possible patient age for a relationship.
+RELATIONSHIP_MIN_AGE: Final = 0
---
+++ opal/hospital_settings/factories.py
@@ -0,0 +1,18 @@
+"""Module providing model factories for hospital settings app models."""
+from factory.django import DjangoModelFactory
+
+from . import models
+
+
+class RelationshipType(DjangoModelFactory):
+    """Model factory to create [opal.hospital_settings.models.RelationshipType][] models."""
+
+    class Meta:
+        model = models.RelationshipType
+
+    name = 'Self'
+    name_fr = 'Soi'
+    description = 'The patient'
+    description_fr = 'Le patient'
+    start_age = 14
+    form_required = False
