a3b43742a910bdaf029f6abff24d6cd3fe156990
Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>
2022-05-17 22:35:24 -0400

    test: Add additional tests and change to read only viewset

--- opal/hospital_settings/tests/test_drf_urls.py
+++ opal/hospital_settings/tests/test_drf_urls.py
@@ -1,5 +1,11 @@
 from django.urls import resolve, reverse
 
+import pytest
+
+from ..models import Institution, Site
+
+pytestmark = pytest.mark.django_db
+
 
 def test_institutions_list() -> None:
     """Ensure institutions list is defined."""
@@ -7,7 +13,21 @@ def test_institutions_list() -> None:
     assert resolve('/api/institutions/').view_name == 'api:institutions-list'
 
 
+def test_institutions_detail(institution: Institution) -> None:
+    """Ensure institutions detail is defined."""
+    path = f'/api/institutions/{institution.pk}/'
+    assert reverse('api:institutions-detail', kwargs={'pk': institution.pk}) == path
+    assert resolve(path).view_name == 'api:institutions-detail'
+
+
 def test_sites_list() -> None:
     """Ensure sites list is defined."""
     assert reverse('api:sites-list') == '/api/sites/'
     assert resolve('/api/sites/').view_name == 'api:sites-list'
+
+
+def test_sites_detail(site: Site) -> None:
+    """Ensure sites detail is defined."""
+    path = f'/api/sites/{site.pk}/'
+    assert reverse('api:sites-detail', kwargs={'pk': site.pk}) == path
+    assert resolve(path).view_name == 'api:sites-detail'
