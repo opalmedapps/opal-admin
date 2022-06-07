27c4af96a0af16eea77411ab264be40a78f5269e
Limin <llmgd@hotmail.com>
2022-06-07 19:45:31 -0400

    Added model SecurityQuestion Added model SecurityAnswer Added functionality Answer Hash to model SecurityAndser Added related test functiosns

--- opal/caregivers/models.py
+++ opal/caregivers/models.py
@@ -1,4 +1,7 @@
 """Module providing models for the caregivers app."""
+from django.conf import settings
+from django.contrib.auth import hashers
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -34,3 +37,165 @@ class CaregiverProfile(models.Model):
             the name of the associated user
         """
         return '{first} {last}'.format(first=self.user.first_name, last=self.user.last_name)
+
+
+class SecurityQuestion(models.Model):
+    """Security question model."""
+
+    question_en = models.CharField(
+        verbose_name=_('Question Text EN'),
+        max_length=2056,
+    )
+
+    question_fr = models.CharField(
+        verbose_name=_('Question Text FR'),
+        max_length=2056,
+    )
+
+    created_at = models.DateField(
+        verbose_name=_('Creation Date'),
+    )
+
+    updated_at = models.DateField(
+        verbose_name=_('Last Updated'),
+    )
+
+    is_active = models.BooleanField(
+        verbose_name=_('Is Active'),
+        default=True,
+    )
+
+    class Meta:
+        verbose_name = _('Security Question')
+        verbose_name_plural = _('Security Questions')
+
+        constraints = [
+            models.CheckConstraint(
+                name='%(app_label)s_%(class)s_date_valid',  # noqa: WPS323
+                check=models.Q(created_at__lte=models.F('updated_at')),
+            ),
+        ]
+
+    def __str__(self) -> str:
+        """Return the question text EN as default.
+
+        Returns:
+            the question text EN as default.
+        """
+        return '{en} {fr}'.format(en=self.question_en, fr=self.question_fr)
+
+    def clean(self) -> None:
+        """Validate if last updated date is earlier creation date.
+
+        Raises:
+            ValidationError: the error shows when updated_at is earlier than created_at
+        """
+        if self.created_at is not None and self.created_at > self.updated_at:
+            raise ValidationError({'created_at': _('Creation date should be earlier than last updated date.')})
+
+
+class SecurityAnswer(models.Model):
+    """Security answer model."""
+
+    HASH_SALT = 'MUHC_MCGILL'
+
+    question = models.ForeignKey(
+        to=SecurityQuestion,
+        verbose_name=_('Security Question'),
+        related_name='security_answers',
+        on_delete=models.CASCADE,
+    )
+
+    profile = models.ForeignKey(
+        to=CaregiverProfile,
+        verbose_name=_('Caregiver Profile'),
+        related_name='security_answers',
+        on_delete=models.CASCADE,
+    )
+
+    answer = models.CharField(
+        verbose_name=_('Security Answer'),
+        max_length=2056,
+    )
+
+    created_at = models.DateField(
+        verbose_name=_('Creation Date'),
+    )
+
+    updated_at = models.DateField(
+        verbose_name=_('Last Updated'),
+    )
+
+    class Meta:
+        verbose_name = _('Security Answer')
+        verbose_name_plural = _('Security Answers')
+
+        constraints = [
+            models.CheckConstraint(
+                name='%(app_label)s_%(class)s_date_valid',  # noqa: WPS323
+                check=models.Q(created_at__lte=models.F('updated_at')),
+            ),
+        ]
+
+    def __str__(self) -> str:
+        """Return the caregiver and the question.
+
+        Returns:
+            the caregiver and the question.
+        """
+        return '{profile} - {question}'.format(profile=self.profile, question=self.question)
+
+    def clean(self) -> None:
+        """Validate if last updated date is earlier creation date.
+
+        Raises:
+            ValidationError: the error shows when updated_at is earlier than created_at
+        """
+        if self.created_at is not None and self.created_at > self.updated_at:
+            raise ValidationError({'created_at': _('Creation date should be earlier than last updated date.')})
+
+    def set_hash_answer(self, raw_answer) -> None:
+        """Set answer text.
+
+        Args:
+            raw_answer: raw_answer the origianl answer text
+        """
+        if not bool(getattr(settings, 'QUESTIONS_CASE_SENSITIVE', False)):
+            raw_answer = raw_answer.upper()
+        # we need to use salt, so that the hash result is not random
+        self.answer = hashers.make_password(raw_answer, self.HASH_SALT)
+
+    def update_answer(self, raw_answer) -> None:
+        """Update answer text.
+
+        Args:
+            raw_answer: raw_answer the origianl answer text
+        """
+        self.set_hash_answer(raw_answer)
+        self.save(update_fields=['answer'])
+
+    def check_answer(self, raw_answer) -> bool:
+        """Check and save answer text. (api function).
+
+        Args:
+            raw_answer: raw_answer the origianl answer text
+
+        Returns:
+            the result of hasers check_password
+        """
+        if not bool(getattr(settings, 'QUESTIONS_CASE_SENSITIVE', False)):
+            raw_answer = raw_answer.upper()
+
+        return hashers.check_password(raw_answer, self.answer, self.update_answer)
+
+    def set_unusable_answer(self) -> str:
+        """Set unusable answer."""
+        self.answer = hashers.make_password(None)
+
+    def has_usable_answer(self) -> bool:
+        """Return the result of the answer is usable or not.
+
+        Returns:
+            the result of the answer is usable or not
+        """
+        return hashers.is_password_usable(self.answer)
--- opal/caregivers/tests/test_models.py
+++ opal/caregivers/tests/test_models.py
@@ -1,12 +1,17 @@
+import datetime
+
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.db.models.deletion import ProtectedError
 
 import pytest
 from pytest_django.asserts import assertRaisesMessage
 
 from opal.users import factories as user_factories
+from opal.users.models import Caregiver
 
-from ..models import CaregiverProfile
+from .. import factories
+from ..models import CaregiverProfile, SecurityAnswer
 
 pytestmark = pytest.mark.django_db
 
@@ -64,3 +69,150 @@ def test_caregiverprofile_legacy_id() -> None:
 
     profile.legacy_id = 1
     profile.full_clean()
+
+
+def test_security_question_str() -> None:
+    """The `str` method returns the name of the security_question."""
+    question = factories.SecurityQuestion()
+    assert str(question) == 'question_one question_un'
+
+
+def test_security_question_factory() -> None:
+    """Ensure the SecurityQuestion factory is building properly."""
+    question = factories.SecurityQuestion()
+    question.full_clean()
+
+
+def test_security_question_active() -> None:
+    """Security Question is active as default."""
+    question = factories.SecurityQuestion()
+
+    assert question.is_active == 1
+
+
+def test_security_question_clean_valid_dates() -> None:
+    """Ensure that the date is valid if created_at is earlier than updated_at."""
+    question = factories.SecurityQuestion(created_at='2022-06-06', updated_at='2022-06-07')
+    question.clean()
+
+
+def test_security_question_clean_invalid_dates() -> None:
+    """Ensure that the date is invalid if created_at is later than updated_at."""
+    question = factories.SecurityQuestion()
+    question.created_at = datetime.date(2022, 6, 8)
+    question.updated_at = datetime.date(2022, 6, 6)
+
+    expected_message = 'Creation date should be earlier than last updated date.'
+    with assertRaisesMessage(ValidationError, expected_message):  # type: ignore[arg-type]
+        question.clean()
+
+
+def test_security_question_dates_constraint() -> None:
+    """Ensure that the date cannot be saved if created_at is later than updated_at."""
+    question = factories.SecurityQuestion()
+    question.created_at = datetime.date(2022, 6, 8)
+    question.updated_at = datetime.date(2022, 6, 6)
+
+    constraint_name = 'caregivers_securityquestion_date_valid'
+    with assertRaisesMessage(IntegrityError, constraint_name):  # type: ignore[arg-type]
+        question.save()
+
+
+def test_security_answer_str() -> None:
+    """The `str` method returns the name of the user and the answer of security answer."""
+    answer = factories.SecurityAnswer()
+    caregiver = user_factories.Caregiver(first_name='first_name', last_name='last_name')
+    profile = CaregiverProfile()
+    profile.user = caregiver
+    answer.profile = profile
+    assert str(answer) == 'first_name last_name - question_one question_un'
+
+
+def test_security_answer_factory() -> None:
+    """Ensure the SecurityAnswer factory is building properly."""
+    answer = factories.SecurityAnswer()
+    answer.full_clean()
+
+
+def test_security_answer_clean_valid_dates() -> None:
+    """Ensure that the date is valid if created_at is earlier than updated_at."""
+    answer = factories.SecurityAnswer(created_at='2022-06-06', updated_at='2022-06-07')
+    answer.clean()
+
+
+def test_security_answer_clean_invalid_dates() -> None:
+    """Ensure that the date is invalid if created_at is later than updated_at."""
+    answer = factories.SecurityAnswer()
+    answer.created_at = datetime.date(2022, 6, 8)
+    answer.updated_at = datetime.date(2022, 6, 6)
+
+    expected_message = 'Creation date should be earlier than last updated date.'
+    with assertRaisesMessage(ValidationError, expected_message):  # type: ignore[arg-type]
+        answer.clean()
+
+
+def test_security_answer_invalid_dates_constraint() -> None:
+    """Ensure that the date cannot be saved if created_at is later than updated_at."""
+    answer = factories.SecurityAnswer()
+    answer.created_at = datetime.date(2022, 6, 8)
+    answer.updated_at = datetime.date(2022, 6, 6)
+
+    constraint_name = 'caregivers_securityanswer_date_valid'
+    with assertRaisesMessage(IntegrityError, constraint_name):  # type: ignore[arg-type]
+        answer.save()
+
+
+def test_security_answer_hash_answer() -> None:
+    """Ensure that the function hash answer is working."""
+    caregiver = user_factories.Caregiver(first_name='first_name', last_name='last_name')
+    text = '123456'
+    answer = SecurityAnswer(
+        question=factories.SecurityQuestion(),
+        profile=CaregiverProfile(user=caregiver),
+    )
+    answer.set_hash_answer(text)
+    assert answer.answer != text
+    str1 = 'argon2$argon2id$v=19$m=102400,t=2,p=8$TVVIQ19NQ0d'
+    str2 = 'JTEw$yn9ImZAb/qJWaSQsecoWQhR3M9evV72fAL0piW2xlug'
+    assert answer.answer == str1 + str2
+
+
+def test_security_answer_update_answer() -> None:
+    """Ensure that the function update answer is working."""
+    caregiver = Caregiver(first_name='first_name', last_name='last_name')
+    caregiver.save()
+    profile = CaregiverProfile(user=caregiver)
+    profile.save()
+    profile.user = caregiver
+    answer = SecurityAnswer(
+        question=factories.SecurityQuestion(),
+        profile=profile,
+    )
+    answer.created_at = datetime.date(2022, 6, 7)
+    answer.updated_at = datetime.date(2022, 6, 7)
+    answer.set_hash_answer('123456')
+    answer.save()
+    str1 = 'argon2$argon2id$v=19$m=102400,t=2,p=8$TVVIQ19NQ0dJ'
+    str2 = 'TEw$yn9ImZAb/qJWaSQsecoWQhR3M9evV72fAL0piW2xlug'
+    assert answer.answer == str1 + str2
+    answer.update_answer('234567')
+    str1 = 'argon2$argon2id$v=19$m=102400,t=2,p=8$TVVIQ19NQ0dJ'
+    str2 = 'TEw$yn9ImZAb/qJWaSQsecoWQhR3M9evV72fAL0piW2xlug'
+    assert answer.answer != str1 + str2
+
+
+def test_security_answer_check_answer() -> None:
+    """Check the raw answer and saved hashed answer, should return True."""
+    question = factories.SecurityQuestion()
+    caregiver = Caregiver(first_name='first_name', last_name='last_name')
+    caregiver.save()
+    profile = CaregiverProfile(user=caregiver)
+    profile.save()
+    profile.user = caregiver
+    text = '123456'
+    answer = SecurityAnswer(question=question, profile=profile)
+    answer.created_at = datetime.date(2022, 6, 7)
+    answer.updated_at = datetime.date(2022, 6, 7)
+    answer.set_hash_answer(text)
+    answer.save()
+    assert answer.check_answer(text)
