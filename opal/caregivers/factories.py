e5483f61bd4af157f21b8cb8597e89c26a3a9745
Limin Liu <limin.liu@muhc.mcgill.ca>
2022-06-30 17:34:31 +0000

    feat: Add model RegistrationCode (QSCCD-163)
    See merge request opalmedapps/opal-admin#137

--- opal/caregivers/factories.py
+++ opal/caregivers/factories.py
@@ -16,3 +16,14 @@ class CaregiverProfile(DjangoModelFactory):
 
     user = SubFactory(Caregiver)
     legacy_id = Sequence(lambda number: number + 1)
+
+
+class RegistrationCode(DjangoModelFactory):
+    """Model factory to create [opal.caregivers.models.RegistrationCode][] models."""
+
+    class Meta:
+        model = models.RegistrationCode
+    # Using string model references to avoid circular import
+    relationship = SubFactory('opal.patients.factories.Relationship')
+    code = 'code12345678'
+    email_verification_code = '123456'
