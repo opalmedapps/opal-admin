c9d94302e57cb04d99791413d0105d74ab271b2c
Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
2022-07-05 19:04:01 +0000

    fix: Change User.language to use project's settings
    See merge request opalmedapps/backend!154

--- opal/users/models.py
+++ opal/users/models.py
@@ -14,17 +14,11 @@ a dedicated profile should be used. This is based on Two Scoops of Django, Secti
 """  # noqa: E501
 from typing import Any
 
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-
-class Language(models.TextChoices):
-    """The available language options for users."""
-
-    ENGLISH = 'EN', _('English')
-    FRENCH = 'FR', _('French')
 
 
 class UserType(models.TextChoices):
@@ -34,6 +28,10 @@ class UserType(models.TextChoices):
     CAREGIVER = 'CAREGIVER', _('Caregiver')
 
 
+# Use the list of languages from the project's settings
+Language = settings.LANGUAGES
+
+
 class User(AbstractUser):
     """Default custom user model."""
 
@@ -41,15 +39,15 @@ class User(AbstractUser):
     # define them as class attributes for easier access
     # see: https://stackoverflow.com/q/71522816
     UserType = UserType
-    Language = Language
 
     base_type = UserType.CLINICAL_STAFF
 
     language = models.CharField(
         verbose_name=_('Language'),
         max_length=2,
-        choices=Language.choices,
-        default=Language.FRENCH,
+        choices=Language,
+        # use the language code of the first language
+        default=Language[0][0],
     )
     phone_number = models.CharField(
         verbose_name=_('Phone Number'),
@@ -70,7 +68,8 @@ class User(AbstractUser):
         constraints = [
             models.CheckConstraint(
                 name='%(app_label)s_%(class)s_language_valid',  # noqa: WPS323
-                check=models.Q(language__in=Language.values),
+                # determine the language codes from the list of tuples
+                check=models.Q(language__in=[language[0] for language in Language]),
             ),
             models.CheckConstraint(
                 name='%(app_label)s_%(class)s_type_valid',  # noqa: WPS323
