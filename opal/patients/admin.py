513844479c9292ff7bbe12d95a987376851f57dd
Stacey Beard <staceybeard10@gmail.com>
2022-07-06 16:58:37 -0400

    Add admin for Patient and Relationship

--- opal/patients/admin.py
+++ opal/patients/admin.py
@@ -3,7 +3,19 @@ from django.contrib import admin
 
 from modeltranslation.admin import TranslationAdmin
 
-from .models import RelationshipType
+from .models import Patient, Relationship, RelationshipType
+
+
+class PatientAdmin(TranslationAdmin):
+    """This class provides admin options for `Patient`."""
+
+    pass  # noqa: WPS420, WPS604
+
+
+class RelationshipAdmin(TranslationAdmin):
+    """This class provides admin options for `Relationship`."""
+
+    pass  # noqa: WPS420, WPS604
 
 
 class RelationshipTypeAdmin(TranslationAdmin):
@@ -12,4 +24,6 @@ class RelationshipTypeAdmin(TranslationAdmin):
     pass  # noqa: WPS420, WPS604
 
 
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Relationship, RelationshipAdmin)
 admin.site.register(RelationshipType, RelationshipTypeAdmin)
--- opal/patients/translation.py
+++ opal/patients/translation.py
@@ -1,7 +1,31 @@
 """This module provides translation options for patient models."""
 from modeltranslation.translator import TranslationOptions, register
 
-from .models import RelationshipType
+from .models import Patient, Relationship, RelationshipType
+
+
+@register(Patient)
+class PatientTranslationOptions(TranslationOptions):
+    """
+    This class provides translation options for `Patient`.
+
+    See [Patient][opal.patients.models.Patient].
+    """
+
+    fields = ()
+    required_languages = ('en', 'fr')
+
+
+@register(Relationship)
+class RelationshipTranslationOptions(TranslationOptions):
+    """
+    This class provides translation options for `Relationship`.
+
+    See [Relationship][opal.patients.models.Relationship].
+    """
+
+    fields = ()
+    required_languages = ('en', 'fr')
 
 
 @register(RelationshipType)
