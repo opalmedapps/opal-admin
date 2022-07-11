1f5fff0c05056e254e4b121d34d3cb4f84572e7a
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2022-07-11 13:23:21 -0400

    test: Update context processor tests

--- opal/core/tests/test_context_processors.py
+++ opal/core/tests/test_context_processors.py
@@ -5,11 +5,21 @@ from pytest_django.fixtures import SettingsWrapper
 from ..context_processors import opal_global_settings
 
 
-def test_opal_admin_processor(settings: SettingsWrapper) -> None:
-    """Ensure that the `opal_admin` context processor returns the correct dictionary."""
+def test_admin_url_in_context_processor(settings: SettingsWrapper) -> None:
+    """Ensure that the `opal_global_settings` context processor returns the OPAL_ADMIN_URL value."""
     url = 'https://example.opal'
     settings.OPAL_ADMIN_URL = url
 
     context = opal_global_settings(HttpRequest())
-    assert all(item in {'OPAL_ADMIN_URL', 'MEDIA_URL'} for item in context)
+    assert 'OPAL_ADMIN_URL' in context
     assert context.get('OPAL_ADMIN_URL') == url
+
+
+def test_media_url_in_context_processor(settings: SettingsWrapper) -> None:
+    """Ensure that the `opal_global_settings` context processor returns the MEDIA_URL value."""
+    url = '/test-media/'
+    settings.MEDIA_URL = url
+
+    context = opal_global_settings(HttpRequest())
+    assert 'MEDIA_URL' in context
+    assert context.get('MEDIA_URL') == url
