08adcdaeeb9e6a363e3b4e93d4ec4a4709ec0af7
Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
2022-07-13 19:10:18 +0000

    fix: Add missing admin sites for models
    See merge request opalmedapps/backend!166

--- opal/caregivers/admin.py
+++ opal/caregivers/admin.py
@@ -1,6 +1,18 @@
 """Module providing admin functionality for the caregivers app."""
 from django.contrib import admin
 
-from .models import CaregiverProfile
+from modeltranslation.admin import TranslationAdmin
 
-admin.site.register(CaregiverProfile, admin.ModelAdmin)
+from . import models
+
+
+class SecurityQuestionAdmin(TranslationAdmin):
+    """This class provides admin options for `SecurityQuestion`."""
+
+    pass  # noqa: WPS420, WPS604
+
+
+admin.site.register(models.CaregiverProfile, admin.ModelAdmin)
+admin.site.register(models.RegistrationCode, admin.ModelAdmin)
+admin.site.register(models.SecurityQuestion, SecurityQuestionAdmin)
+admin.site.register(models.SecurityAnswer, admin.ModelAdmin)
