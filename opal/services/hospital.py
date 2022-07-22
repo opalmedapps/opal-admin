4caceb21bd241615590e3378beab6b1090505968
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2022-07-22 15:24:55 -0400

    feat: Add OIECommunicationService

--- opal/services/hospital.py
+++ opal/services/hospital.py
@@ -1,6 +1,7 @@
 """Module providing business logic for the hospital's internal communicatoin (e.g., Opal Integration Engine)."""
 
 import json
+import re
 from datetime import datetime
 from http import HTTPStatus
 from typing import NamedTuple
@@ -12,32 +13,46 @@ import requests
 from requests.auth import HTTPBasicAuth
 from requests.exceptions import RequestException
 
+from opal.utils.base64_util import Base64Util
 
-class QuestionnairePDFReport(NamedTuple):
-    """Typed `NamedTuple` that describes data fields needed for exporting questionnaire PDF report to the OIE."""
 
-    mrn: str                 # one of the patient's MRNs for the site
-    site: str                # one of the patient's site code for the MRN
-    base64_content: str      # the base64-encoded PDF
-    document_type: str       # the document number (e.g., FMU-... or MU-...)
-    document_date: datetime  # the datetime in YYYY-MM-DD HH:II:SS
+class OIEReportExportData(NamedTuple):
+    """Typed `NamedTuple` that describes data fields needed for exporting a PDF report to the OIE.
+
+    Attributes:
+        mrn (str): one of the patient's MRNs for the site
+        site (str): one of the patient's site code for the MRN
+        base64_content (str): the base64-encoded PDF (e.g., questionnaire PDF report)
+        document_type (str): the document number (e.g., FMU-... or MU-...)
+        document_date (datetime): the datetime in YYYY-MM-DD HH:II:SS
+    """
+
+    mrn: str
+    site: str
+    base64_content: str
+    document_type: str
+    document_date: datetime
 
 
 class OIECommunicationService:
     """Service that provides functionality for communication with Opal Integration Engine (OIE)."""
 
-    def export_questionnaire_report(
+    def export_pdf_report(
         self,
-        report_data: QuestionnairePDFReport,
+        report_data: OIEReportExportData,
     ) -> JsonResponse:
-        """Send base64 encoded questionnaire PDF report to the OIE.
+        """Send base64 encoded PDF report to the OIE.
 
         Args:
-            report_data (QuestionnairePDFReport): questionnaire PDF report data needed to call OIE endpoint
+            report_data (OIEReportExportData): PDF report data needed to call OIE endpoint
 
         Returns:
             JsonResponse: HTTP JSON response
         """
+        # return a `JsonResponse` with a BAD_REQUEST if `OIEReportExportData` is not valid
+        if not self._is_report_export_data_valid(report_data):
+            return JsonResponse({'status': HTTPStatus.BAD_REQUEST, 'message': 'invalid export data'})
+
         pload = json.dumps({
             'mrn': report_data.mrn,
             'site': report_data.site,
@@ -66,3 +81,27 @@ class OIECommunicationService:
             return JsonResponse({'status': HTTPStatus.BAD_REQUEST, 'message': str(decode_err)})
 
         return JsonResponse(json_data)
+
+    def _is_report_export_data_valid(self, report_data: OIEReportExportData) -> bool:
+        """Check if OIE report export data is valid.
+
+        Args:
+            report_data (OIEReportExportData): OIE report export data needed to call OIE endpoint
+
+        Returns:
+            bool: boolean value showing if OIE report export data is valid
+        """
+        # check if mrn exists
+        # if LegacyPatientHospitalIdentifier.objects.filter(MRN=report_data.mrn).exists()
+
+        # check if site exists
+        # if LegacyPatientHospitalIdentifier.objects.filter(
+        # Hospital_Identifier_Type_Code=report_data.site
+        # ).exists()
+
+        return (  # check if report content is base64
+            Base64Util().base64_util.is_base64(report_data.base64_content)
+            # check if document type format is valid?
+            and re.compile('(^FU-[a-zA-Z]+$)|(^FMU-[a-zA-Z]+$)')
+            # check document date?
+        )
