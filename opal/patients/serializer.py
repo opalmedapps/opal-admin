a34d9f00acf213f7ebad67ab8165b29ba4bd82c5
David Gagne <david.gagne@muhc.mcgill.ca>
2022-08-02 12:23:17 -0400

    WIP Attempt at getting status fo a relationship

--- opal/patients/serializer.py
+++ opal/patients/serializer.py
@@ -1,7 +1,9 @@
 """Module that contains serializer classes for patient models."""
+from typing import Union
+
 from rest_framework import serializers
 
-from opal.patients.models import HospitalPatient, Patient, Relationship
+from opal.patients.models import HospitalPatient, Patient
 
 
 class PatientRegistrationSerializer(serializers.ModelSerializer):
@@ -20,23 +22,23 @@ class HospitalPatientRegistrationSerializer(serializers.ModelSerializer):
         fields = ['mrn', 'is_active']
 
 
-class RelationshipStatusSerializer(serializers.ModelSerializer):
-    """Serializer to return patient caregive relationship status."""
-
-    class Meta:
-        model = Relationship
-        fields = ['status']
-
-
 class CaregiverPatientListSerializer(serializers.ModelSerializer):
     """Serializer for the list of patient for a given caregiver."""
 
-    status = RelationshipStatusSerializer(
-        source='caregivers.relationships',
-        many=True,
-        read_only=True,
-    )
+    status = serializers.SerializerMethodField()
 
     class Meta:
         model = Patient
         fields = ['id', 'legacy_id', 'first_name', 'last_name', 'status']
+
+    def get_status(self, patient: Patient) -> Union[str, None]:
+        """
+        Get status of the relationship.
+
+        Args:
+            patient: Patient object related to the given caregiver.
+
+        Returns:
+            Status of the relationship
+        """
+        return patient.relationships.first().status
