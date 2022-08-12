8fd30376368d6cd870e752841f85aa449042ff86
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2022-08-12 13:23:43 -0400

    refactor: Fixing imports

--- opal/legacy/api/views.py
+++ opal/legacy/api/views.py
@@ -1,34 +1,29 @@
 """Collection of api views used to send data to opal app through the listener request relay."""
 
 import datetime
+from pathlib import Path
 from typing import Any
 
 from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse
-from django.utils import timezone
-from django.utils.translation import override
+from django.utils import timezone, translation
 
-from rest_framework import status
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, permissions, response, status, views
 from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from opal.hospital_settings.models import Institution
 from opal.legacy.utils import get_patient_sernum
-from opal.patients.factories import HospitalPatient
-from opal.services.hospital import OIEReportExportData, OIEService
-from opal.services.reports import QuestionnaireReportRequestData, ReportService
+from opal.patients.models import HospitalPatient
+from opal.services import hospital, reports
 
 from ..models import LegacyAppointment, LegacyNotification
 from .serializers import LegacyAppointmentSerializer, QuestionnaireReportRequestSerializer
 
 
-class AppHomeView(APIView):
+class AppHomeView(views.APIView):
     """Class to return home page required data."""
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request: HttpRequest) -> HttpResponse:
         """
@@ -41,7 +36,7 @@ class AppHomeView(APIView):
             Http response with the data needed to display the home view.
         """
         patient_sernum = get_patient_sernum(request.headers['Appuserid'])
-        return Response({
+        return response.Response({
             'unread_notification_count': self.get_unread_notification_count(patient_sernum),
             'daily_appointments': LegacyAppointmentSerializer(
                 self.get_daily_appointments(patient_sernum),
@@ -84,13 +79,18 @@ class AppHomeView(APIView):
         return LegacyNotification.objects.filter(patientsernum=sernum, readstatus=0).count()
 
 
-class QuestionnairesReportCreateAPIView(CreateAPIView):
+class QuestionnairesReportCreateAPIView(generics.CreateAPIView):
     """View to generate a questionnaires PDF report."""
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = QuestionnaireReportRequestSerializer
 
-    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+    def post(
+        self,
+        request: Request,
+        *args: Any,
+        **kwargs: Any,
+    ) -> response.Response:
         """
         Generate questionnaire PDF report and submit to the OIE.
 
@@ -102,23 +102,23 @@ class QuestionnairesReportCreateAPIView(CreateAPIView):
         Returns:
             HTTP `Response` with results of report generation
         """
-        with override(request.headers['Accept-Language']):  # TODO: validate and override the language in a middleware
+        with translation.override(request.headers['Accept-Language']):  # TODO: override the language in a middleware
             serializer = QuestionnaireReportRequestSerializer(data=request.data)
             # Validate received data. Return a 400 response if the data was invalid.
             serializer.is_valid(raise_exception=True)
 
             # Generate questionnaire report
-            report_service = ReportService()
+            report_service = reports.ReportService()
             encoded_report = report_service.generate_questionnaire_report(
-                QuestionnaireReportRequestData(
-                    patient_id=serializer.validated_data.patient_id,
-                    logo_path=Institution.objects.get(pk=1).logo.path,
-                    language=serializer.validated_data.language,
+                reports.QuestionnaireReportRequestData(
+                    patient_id=int(request.data['patient_id']),
+                    logo_path=Path(Institution.objects.get(pk=1).logo.path),
+                    language=request.headers['Accept-Language'],
                 ),
             )
 
             if encoded_report == '':
-                return Response(
+                return response.Response(
                     {
                         'status': 'error',
                         'message': 'An error occured during report generation.',
@@ -127,9 +127,9 @@ class QuestionnairesReportCreateAPIView(CreateAPIView):
                 )
 
             # Submit report to the OIE
-            oie = OIEService()
-            export_result = oie.export_questionnaire_report(
-                OIEReportExportData(
+            oie = hospital.OIEService()
+            export_result = oie.export_pdf_report(
+                hospital.OIEReportExportData(
                     mrn=HospitalPatient.objects.filter(
                         patient__legacy_id=serializer.validated_data.patient_id,
                     ).first().mrn,
@@ -137,9 +137,9 @@ class QuestionnairesReportCreateAPIView(CreateAPIView):
                         patient__legacy_id=serializer.validated_data.patient_id,
                     ).first().site.name,
                     base64_content=encoded_report,
-                    document_type='FMU',  # TODO: clarify where to get the value
+                    document_number='FMU',  # TODO: clarify where to get the value
                     document_date=datetime.datetime.now(),  # TODO: get the exact time of the report creation
                 ),
             )
 
-            return Response(export_result)
+            return response.Response(export_result)
