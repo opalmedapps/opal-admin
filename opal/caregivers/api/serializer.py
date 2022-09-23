87a0f7455fb4f70efc53f3b307384406a3db9d67
lymanliugd <Limin.Liu@muhc.mcgill.ca>
2022-09-23 08:30:45 -0400

    feat deps refactor the serializers

--- opal/caregivers/api/serializer.py
+++ opal/caregivers/api/serializer.py
@@ -1,8 +1,16 @@
 """This module provides Django REST framework serializers for GetRegistrationEncryptionInfoView api return value."""
+from typing import Any, Dict
+
 from rest_framework import serializers
 
 from opal.caregivers.models import RegistrationCode
-from opal.patients.api.serializers import HospitalPatientRegistrationSerializer, PatientSerializer
+from opal.hospital_settings.api.serializers import InstitutionSerializer
+from opal.hospital_settings.models import Institution
+from opal.patients.api.serializers import (
+    HospitalPatientAndSiteSerializer,
+    HospitalPatientRegistrationSerializer,
+    PatientSerializer,
+)
 
 
 class RegistrationEncryptionInfoSerializer(serializers.ModelSerializer):
@@ -23,3 +31,50 @@ class RegistrationEncryptionInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = RegistrationCode
         fields = ['code', 'status', 'patient', 'hospital_patients']
+
+
+class RegistrationCodePatientSerializer(serializers.ModelSerializer):
+    """Serializer for the return summary info of registration code."""
+
+    patient = PatientSerializer(
+        source='relationship.patient',
+        fields=('first_name', 'last_name'),
+        many=False,
+        read_only=True,
+    )
+    institution = serializers.SerializerMethodField()
+
+    def get_institution(self, obj: Any) -> Dict:  # noqa: WPS615
+        """
+        Get a single institution data.
+
+        Args:
+            obj (Any): Any.
+
+        Returns:
+            return institution data with Dict.
+        """
+        return InstitutionSerializer(Institution.objects.get(), fields=('id', 'name')).data
+
+    class Meta:
+        model = RegistrationCode
+        fields = ['patient', 'institution']
+
+
+class RegistrationCodePatientDetailedSerializer(serializers.ModelSerializer):
+    """Serializer for the return detailed info of registration code."""
+
+    patient = PatientSerializer(
+        source='relationship.patient',
+        many=False,
+        read_only=True,
+    )
+    hospital_patients = HospitalPatientAndSiteSerializer(
+        source='relationship.patient.hospital_patients',
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = RegistrationCode
+        fields = ['patient', 'hospital_patients']
--- opal/patients/api/serializers.py
+++ opal/patients/api/serializers.py
@@ -1,7 +1,6 @@
 """This module provides Django REST framework serializers for PatietnView api return value."""
 from rest_framework import serializers
 
-from opal.caregivers.models import RegistrationCode
 from opal.core.api.serializers import DynamicFieldsSerializer
 from opal.patients.models import HospitalPatient, Patient, Relationship
 
@@ -40,8 +39,8 @@ class HospitalPatientInstitutionSerializer(serializers.ModelSerializer):
         fields = ['institution_id', 'name']
 
 
-class HospitalPatientSiteSerializer(serializers.ModelSerializer):
-    """Hospital patient serializer used to get site information."""
+class HospitalPatientAndSiteSerializer(serializers.ModelSerializer):
+    """Hospital patient serializer which also provides the site code."""
 
     site_code = serializers.CharField(
         source='site.code',
@@ -64,42 +63,3 @@ class CaregiverPatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Relationship
         fields = ['patient_id', 'patient_legacy_id', 'first_name', 'last_name', 'status']
-
-
-class RegistrationCodePatientSerializer(serializers.ModelSerializer):
-    """Serializer for the return summary info of registration code."""
-
-    patient = PatientSerializer(
-        source='relationship.patient',
-        fields=('first_name', 'last_name'),
-        many=False,
-        read_only=True,
-    )
-    institutions = HospitalPatientInstitutionSerializer(
-        source='relationship.patient.hospital_patients',
-        many=True,
-        read_only=True,
-    )
-
-    class Meta:
-        model = RegistrationCode
-        fields = ['patient', 'institutions']
-
-
-class RegistrationCodePatientDetailedSerializer(serializers.ModelSerializer):
-    """Serializer for the return detailed info of registration code."""
-
-    patient = PatientSerializer(
-        source='relationship.patient',
-        many=False,
-        read_only=True,
-    )
-    hosptial_patients = HospitalPatientSiteSerializer(
-        source='relationship.patient.hospital_patients',
-        many=True,
-        read_only=True,
-    )
-
-    class Meta:
-        model = RegistrationCode
-        fields = ['patient', 'hosptial_patients']
