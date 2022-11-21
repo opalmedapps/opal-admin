a14ad4dae1b78795bf99b53c9b9db71847f640bc
Ridhi Mittal <99695447+ridhimittal3@users.noreply.github.com>
2022-11-21 16:52:30 -0500

    feat: Add can_answer_questionnaire field

--- opal/patients/migrations/0008_can_answer_questionnaire.py
+++ opal/patients/migrations/0008_can_answer_questionnaire.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-    """Add `can_answer_questionnaires` field to RelationshipType model."""
+    """Add `can_answer_questionnaire` field to RelationshipType model."""
 
     dependencies = [
         ('patients', '0007_patient_hin_to_ramq'),
