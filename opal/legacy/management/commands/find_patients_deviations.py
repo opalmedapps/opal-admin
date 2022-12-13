802e30f56a816f6579afbed6a4abb4839853d94d
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2022-12-13 12:55:11 -0500

    feat: add custom command to find patient model deviations

--- opal/legacy/management/commands/find_patients_deviations.py
+++ opal/legacy/management/commands/find_patients_deviations.py
@@ -13,13 +13,13 @@ class Command(BaseCommand):
         - `OpalDB.Patient` table against `opal.patients_patient` table
         - `OpalDB.Patient_Hospital_Identifier` table against `opal.patients_hospitalpatient` table
 
-    by running raw SQL queries.
+    by using Django's models.
     """
 
     help = 'Check the legacy and new back end databases for the data deviations in the `Patient` tables.'  # noqa: A003
     requires_migrations_checks = True
 
-    def handle(self, *args: Any, **kwargs: Any) -> None:
+    def handle(self, *args: Any, **kwargs: Any) -> None:  # noqa: WPS210
         """
         Handle deviation check for the `Patient` model tables.
 
@@ -35,9 +35,41 @@ class Command(BaseCommand):
             args: input arguments.
             kwargs: input arguments.
         """
-        with connections['default'].cursor() as django_conn:
-            with connections['legacy'].cursor() as legacy_conn:
-                query = """
+        with connections['default'].cursor() as django_db:
+            patient_query = """
+                SELECT
+                    PP.legacy_id AS LegacyID,
+                    PP.ramq AS RAMQ,
+                    PP.first_name AS FirstName,
+                    PP.last_name AS LastName,
+                    DATE_FORMAT(PP.date_of_birth, "%Y-%m-%d") AS BirthDate,
+                    UPPER(PP.sex) AS Sex,
+                    UU.phone_number AS Phone,
+                    LOWER(UU.email) AS Email,
+                    LOWER(UU.language) AS Language
+                FROM patients_patient PP
+                LEFT JOIN caregivers_caregiverprofile CC ON PP.legacy_id = CC.legacy_id
+                LEFT JOIN users_user UU ON CC.user_id = UU.id;
+            """  # noqa: WPS323
+
+            hospital_patient_query = """
+            SELECT
+                PP.legacy_id AS LegacyID,
+                UPPER(HSS.code) AS SiteCode,
+                PHP.mrn AS MRN,
+                PHP.is_active AS IsActive
+            FROM patients_hospitalpatient PHP
+            LEFT JOIN patients_patient PP ON PHP.id = PP.id
+            LEFT JOIN hospital_settings_site HSS ON PHP.site_id = HSS.id;
+            """
+
+            django_db.execute(patient_query)
+            django_patient_set = set(django_db.fetchall())
+            django_db.execute(hospital_patient_query)
+            django_hospital_patient = set(django_db.fetchall())
+
+        with connections['legacy'].cursor() as legacy_db:
+            patient_query = """
                 SELECT
                     PatientSerNum AS LegacyID,
                     SSN AS RAMQ,
@@ -52,17 +84,36 @@ class Command(BaseCommand):
                         WHEN UPPER(Sex) = "FEMALE" THEN "F"
                         ELSE "UNDEFINED"
                     END
-                    ) AS Gender,
+                    ) AS Sex,
                     TelNum AS Phone,
                     LOWER(Email) AS Email,
                     LOWER(Language) AS Language
-                FROM Patient;"""
-                legacy_conn.execute(query)
+                FROM Patient;
+            """  # noqa: WPS323
 
-                print(legacy_conn.fetchall())
-                self.stdout.write('hello world!')
-        # self.stderr.write(
-        #     'Patient with sernum: {legacy_id}, does not exist,skipping.'.format(
-        #         legacy_id=legacy_user.usertypesernum,
-        #     ),
-        # )
+            hospital_patient_query = """
+                SELECT
+                    PatientSerNum AS LegacyID,
+                    UPPER(Hospital_Identifier_Type_Code) AS SiteCode,
+                    MRN AS MRN,
+                    Is_Active AS IsActive
+                FROM Patient_Hospital_Identifier;
+            """
+
+            legacy_db.execute(patient_query)
+            legacy_patient = legacy_db.fetchall()
+            legacy_db.execute(hospital_patient_query)
+            legacy_hospital_patient = legacy_db.fetchall()
+
+        # Please see the details about the `symmetric_difference` method in the links below:
+        # https://www.geeksforgeeks.org/python-set-symmetric_difference-2/
+        # https://www.w3schools.com/python/ref_set_symmetric_difference.asp
+        unmatched_patients = django_patient_set.symmetric_difference(legacy_patient)
+
+        unmatched_hospital_patients = django_hospital_patient.symmetric_difference(legacy_hospital_patient)
+
+        self.stderr.write(
+            '\n'.join(str(patient) for patient in (unmatched_patients)),
+        )
+
+        self.stdout.write(str(unmatched_hospital_patients))
