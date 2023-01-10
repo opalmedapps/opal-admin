860b43b1c5059a6c7a432e4902fa13d1162ac6b0
Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>
2023-01-10 00:38:51 -0500

    test: add lots of tests

--- opal/health_data/migrations/0001_initial.py
+++ opal/health_data/migrations/0001_initial.py
@@ -1,3 +1,4 @@
+import django.core.validators
 from django.db import migrations, models
 import django.db.models.deletion
 
@@ -36,8 +37,8 @@ class Migration(migrations.Migration):
                 ('device', models.CharField(help_text='The device that was used to take the measurement', max_length=255, verbose_name='Device')),
                 ('source', models.CharField(choices=[('P', 'Patient'), ('C', 'Clinician')], help_text='The source that provided this sample, for example, the patient if it is patient-reported data', max_length=1, verbose_name='Source')),
                 ('added_at', models.DateTimeField(auto_now_add=True, verbose_name='Added At')),
-                ('value', models.DecimalField(decimal_places=2, max_digits=7, verbose_name='Value')),
-                ('type', models.CharField(choices=[('BM', 'Body Mass (kg)'), ('TMP', 'Body Temperature (°C)'), ('HR', 'Heart Rate (bpm)'), ('HRV', 'Heart Rate Variability (ms))'), ('SPO2', 'Oxygen Saturation (%)')], max_length=4, verbose_name='Type')),
+                ('value', models.DecimalField(decimal_places=2, max_digits=7, validators=[django.core.validators.MinValueValidator(0)], verbose_name='Value')),
+                ('type', models.CharField(choices=[('BM', 'Body Mass (kg)'), ('TMP', 'Body Temperature (°C)'), ('HR', 'Heart Rate (bpm)'), ('HRV', 'Heart Rate Variability (ms)'), ('SPO2', 'Oxygen Saturation (%)')], max_length=4, verbose_name='Type')),
                 ('data_store', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='quantity_samples', to='health_data.healthdatastore', verbose_name='Health Data Store')),
             ],
             options={
@@ -48,5 +49,9 @@ class Migration(migrations.Migration):
         migrations.AddConstraint(
             model_name='quantitysample',
             constraint=models.CheckConstraint(check=models.Q(('type__in', ['BM', 'TMP', 'HR', 'HRV', 'SPO2'])), name='health_data_quantitysample_type_valid'),
+        ),
+        migrations.AddConstraint(
+            model_name='healthdatastore',
+            constraint=models.UniqueConstraint(fields=('patient',), name='health_data_healthdatastore_unique_patient'),
         ),
     ]
