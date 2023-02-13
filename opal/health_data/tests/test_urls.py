e5180251f9789d9a5d11b0f13eadd05f02c5dbc0
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2023-02-13 15:32:05 +0000

    refactor: replace patient id with uuid for health-data UI
    See merge request opalmedapps/backend!381

--- opal/health_data/tests/test_urls.py
+++ opal/health_data/tests/test_urls.py
@@ -1,8 +1,11 @@
+from uuid import uuid4
+
 from django.urls import resolve, reverse
 
 
 def test_health_data_ui_url_exists() -> None:
     """A URL for wearables UI page exists."""
-    url = '/health-data/1/quantity-samples/'
-    assert reverse('health_data:health-data-ui', kwargs={'id': 1}) == url
+    uuid = uuid4()
+    url = '/health-data/{uuid}/quantity-samples/'.format(uuid=uuid)
+    assert reverse('health_data:health-data-ui', kwargs={'uuid': uuid}) == url
     assert resolve(url).view_name == 'health_data:health-data-ui'
