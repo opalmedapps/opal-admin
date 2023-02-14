99d72fe4d0ce4abd8de2a568b05bdbd9e2df6971
Napparific <kelly.agnew@mail.mcgill.ca>
2023-02-14 14:44:38 -0500

    fix: automatically update minimum allowed value for enddate if startdate changed

--- opal/static/js/query.js
+++ opal/static/js/query.js
@@ -32,3 +32,10 @@ questionCBox.addEventListener('change', e => {
         }
     }
 });
+
+
+function updateEndDate() {
+    var start_date = document.getElementById("start").value;
+    document.getElementById("end").value = "";
+    document.getElementById("end").setAttribute("min",start_date);
+}
