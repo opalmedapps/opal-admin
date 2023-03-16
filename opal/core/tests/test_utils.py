4cc8ed9a53c9005ac00e96b8eb47c8f34fda7912
Zeyu Dou <dou3180@hotmail.com>
2023-03-16 16:43:26 -0400

    fix the unit test

--- opal/core/tests/test_utils.py
+++ opal/core/tests/test_utils.py
@@ -24,8 +24,9 @@ def test_random_uuid_is_unique() -> None:
 def test_random_registration_code_format() -> None:
     """Ensure if random registration code is alpha-numeric string and equal to the given length."""
     length = 10
-    code = generate_random_registration_code(length)
-    assert len(code) == length
+    test_institution_code = 'XX'
+    code = generate_random_registration_code(test_institution_code, length)
+    assert len(code) == length + len(test_institution_code)
     assert isinstance(code, str)
     assert code.isalnum()
 
