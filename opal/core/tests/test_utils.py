5d45be1c8d090bc973660a8c269165ef7b453c67
Zeyu Dou <dou3180@hotmail.com>
2023-03-20 13:43:19 +0000

    fix: Add institution code prefix to registration code
    See merge request opalmedapps/backend!454

--- opal/core/tests/test_utils.py
+++ opal/core/tests/test_utils.py
@@ -24,14 +24,15 @@ def test_random_uuid_is_unique() -> None:
 def test_random_registration_code_format() -> None:
     """Ensure if random registration code is alpha-numeric string and equal to the given length."""
     length = 10
-    code = generate_random_registration_code(length)
-    assert len(code) == length
+    test_institution_code = 'XX'
+    code = generate_random_registration_code(test_institution_code, length)
+    assert len(code) == length + len(test_institution_code)
     assert isinstance(code, str)
     assert code.isalnum()
 
 
 def test_random_registration_code_is_unique() -> None:
     """Ensure the random registration code is unique."""
-    code1 = generate_random_uuid(10)
-    code2 = generate_random_uuid(10)
+    code1 = generate_random_registration_code('XX', 10)
+    code2 = generate_random_registration_code('XX', 10)
     assert code1 != code2
