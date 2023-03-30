6c01cab7405041b6c6566e66360d77dc6ea00327
Matthias Schoettle <git@mattsch.com>
2023-03-30 12:13:07 -0400

    fix: detect active menu item for sub pages

---
+++ opal/core/templatetags/extra_filters.py
@@ -0,0 +1,45 @@
+"""
+Module providing extra filters for Django templates.
+
+Import them into your template with:
+
+```jinja2
+{% load extra_filters %}
+```
+"""
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+
+@register.filter('rsubstring')
+@stringfilter
+def rsubstring(text: str, separator: str) -> str:
+    """
+    Return the substring cut off at the right-most separator.
+
+    Args:
+        text: the input text
+        separator: the separator at which to split the text
+
+    Returns:
+        the substring cut off at the right-most separator, empty string if the separator is not present
+    """
+    return text.rpartition(separator)[0]
+
+
+@register.filter('startswith')
+@stringfilter
+def startswith(text: str, prefix: str) -> bool:
+    """
+    Return whether the text starts with a given prefix.
+
+    Args:
+        text: the input text
+        prefix: the prefix to search for
+
+    Returns:
+        True, if the text starts with the expected prefix, False otherwise
+    """
+    return text.startswith(prefix)
