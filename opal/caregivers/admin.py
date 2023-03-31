888cadc9a0e8adcd845dba77fc87e3fb8434ca6b
Matthias Schoettle <git@mattsch.com>
2023-03-31 13:03:25 +0000

    fix: show UUID for patient and caregiver profile in admin
    See merge request opalmedapps/opal-admin#478

--- opal/caregivers/admin.py
+++ opal/caregivers/admin.py
@@ -6,14 +6,26 @@ from modeltranslation.admin import TranslationAdmin
 from . import models
 
 
+class CaregiverProfileAdmin(admin.ModelAdmin):
+    """Admin options for the `CaregiverProfile` model."""
+
+    readonly_fields = ['uuid']
+
+
+class RegistrationCodeAdmin(admin.ModelAdmin):
+    """Admin options for the `RegistrationCode` model."""
+
+    readonly_fields = ['created_at']
+
+
 class SecurityQuestionAdmin(TranslationAdmin):
     """This class provides admin options for `SecurityQuestion`."""
 
     pass  # noqa: WPS420, WPS604
 
 
-admin.site.register(models.CaregiverProfile, admin.ModelAdmin)
-admin.site.register(models.RegistrationCode, admin.ModelAdmin)
+admin.site.register(models.CaregiverProfile, CaregiverProfileAdmin)
+admin.site.register(models.RegistrationCode, RegistrationCodeAdmin)
 admin.site.register(models.SecurityQuestion, SecurityQuestionAdmin)
 admin.site.register(models.SecurityAnswer, admin.ModelAdmin)
 admin.site.register(models.Device, admin.ModelAdmin)
