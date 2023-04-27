ea7b6ed57e7f84da2f0b4ed692642acad765fb2e
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2023-04-27 17:23:22 +0000

    feat: add shared data and DataType models to databank app
    See merge request opalmedapps/backend!505

--- opal/databank/tests/test_models.py
+++ opal/databank/tests/test_models.py
@@ -1,6 +1,13 @@
+
+from django.db import IntegrityError
+
 import pytest
+from pytest_django.asserts import assertRaisesMessage
+
+from opal.patients.factories import Patient
 
 from .. import factories
+from ..models import SharedData
 
 pytestmark = pytest.mark.django_db()
 
@@ -12,9 +19,46 @@ def test_databankconsent_factory() -> None:
     databank_consent.full_clean()
 
 
-def test_quantitysample_str() -> None:
+def test_databankconsent_str() -> None:
     """Ensure the `__str__` method is defined for the `DatabankConsent` model."""
     databank_consent = factories.DatabankConsent()
     databank_consent.full_clean()
 
     assert str(databank_consent) == "Bart Simpson's Databank Consent"
+
+
+def test_shareddata_factory() -> None:
+    """Ensure the `SharedData` factory creates a valid model."""
+    shared_data = factories.SharedData()
+
+    shared_data.full_clean()
+
+
+def test_shareddata_str() -> None:
+    """Ensure the `__str__` method is defined for the `SharedData` model."""
+    shared_data = factories.SharedData()
+    shared_data.full_clean()
+
+    assert str(shared_data) == f'{shared_data.get_data_type_display()} datum, sent at {shared_data.sent_at}'
+
+
+def test_sharedata_datatype_constraint() -> None:
+    """Ensure the valid choices for the shared data's `type` are validated using a constraint."""
+    databank_consent = factories.DatabankConsent()
+    shared_data = factories.SharedData.build(databank_consent=databank_consent, data_type='INV')
+
+    constraint_name = 'databank_shareddata_data_type_valid'
+    with assertRaisesMessage(IntegrityError, constraint_name):
+        shared_data.save()
+
+
+def test_shareddata_multiple_per_patient() -> None:
+    """Ensure a patient (via a DatabankConsent) can have multiple instances of SharedData."""
+    patient = Patient()
+    databank_consent = factories.DatabankConsent(patient=patient)
+
+    factories.SharedData(databank_consent=databank_consent)
+    factories.SharedData(databank_consent=databank_consent)
+    factories.SharedData(databank_consent=databank_consent)
+
+    assert SharedData.objects.count() == 3
