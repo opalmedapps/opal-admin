bb3686ba202c4d58fe61e8548740b31655483ded
Gladyr Anton <anton.gladyr@muhc.mcgill.ca>
2023-07-07 16:04:28 +0000

    feat: add management command to update patient UUIDs in ORMS
    See merge request opalmedapps/backend!655

--- opal/core/test_utils.py
+++ opal/core/test_utils.py
@@ -1,8 +1,14 @@
 """Utility classes used by management commands or in the testing of management commands."""
+import json
+from http import HTTPStatus
 from io import StringIO
 from typing import Any
+from unittest.mock import MagicMock
 
 from django.core.management import call_command
+
+import requests
+from pytest_mock.plugin import MockerFixture
 
 
 class CommandTestMixin:
@@ -30,3 +36,55 @@ class CommandTestMixin:
             **kwargs,
         )
         return out.getvalue(), err.getvalue()
+
+
+class RequestMockerTest:
+    """Class that provides methods to mock HTTP requests."""
+
+    @classmethod
+    def mock_requests_post(
+        cls,
+        mocker: MockerFixture,
+        response_data: dict[str, Any],
+    ) -> MagicMock:
+        """Mock an HTTP POST call to a web service.
+
+        Args:
+            mocker: object that provides the same interface to functions in the mock module
+            response_data: generated mock response data
+
+        Returns:
+            object that mocks HTTP post request to the web service
+        """
+        mock_post = mocker.patch('requests.post')
+        response = requests.Response()
+        response.status_code = HTTPStatus.OK
+
+        response._content = json.dumps(response_data).encode()
+        mock_post.return_value = response
+
+        return mock_post
+
+    @classmethod
+    def mock_requests_get(
+        cls,
+        mocker: MockerFixture,
+        generated_response_data: dict[str, str],
+    ) -> MagicMock:
+        """Mock an HTTP GET call to a web service.
+
+        Args:
+            mocker: object that provides the same interface to functions in the mock module
+            generated_response_data: generated mock response data
+
+        Returns:
+            object that mocks HTTP get request to the web service
+        """
+        mock_get = mocker.patch('requests.get')
+        response = requests.Response()
+        response.status_code = HTTPStatus.OK
+
+        response._content = json.dumps(generated_response_data).encode()
+        mock_get.return_value = response
+
+        return mock_get
