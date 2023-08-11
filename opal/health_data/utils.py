fb4ad6354e19e863e5f4aa859056d635e7a4dc3f
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2023-08-11 14:31:01 +0000

    feat: show systolic and diastolic blood pressure measurements on the same chart
    See merge request opalmedapps/backend!708

---
+++ opal/health_data/utils.py
@@ -0,0 +1,93 @@
+"""Utility functions used by health data app."""
+from typing import Final, Optional
+
+from django.conf import settings
+from django.utils.translation import gettext
+
+import pandas as pd
+
+from opal.patients.models import Patient
+from opal.services.charts import ChartData, ChartService
+
+from .models import QuantitySample, QuantitySampleType
+
+BLOOD_PRESSURE_SAMPLE_TYPES: Final = (
+    QuantitySampleType.BLOOD_PRESSURE_SYSTOLIC,
+    QuantitySampleType.BLOOD_PRESSURE_DIASTOLIC,
+)
+
+SINGLE_VALUE_SAMPLE_TYPES: Final = tuple(
+    sample for sample in QuantitySampleType if sample not in BLOOD_PRESSURE_SAMPLE_TYPES
+)
+
+
+def build_all_quantity_sample_charts(patient: Patient) -> dict[str, Optional[str]]:
+    """Build all the quantity sample charts for a specific patient.
+
+    Args:
+        patient: patient for whom charts are being generated
+
+    Returns:
+        dictionary of the quantity sample charts in HTML format
+    """
+    charts: dict[str, Optional[str]] = {}
+    chart_service = ChartService()
+
+    # Build charts for the measurements that contain only one value
+    for sample_type in SINGLE_VALUE_SAMPLE_TYPES:
+        df = pd.DataFrame(
+            data=QuantitySample.objects.order_by('start_date').filter(
+                patient=patient,
+                type=sample_type,
+            ).values('start_date', 'value', 'device'),
+        )
+
+        if df.empty:
+            charts[sample_type.label.split(' (')[0]] = None
+            continue
+
+        # By default the timezone in the dataframe is UTC.
+        # Should be set to the local timezone: https://stackoverflow.com/a/50062101
+        df['start_date'] = df['start_date'].dt.tz_convert(settings.TIME_ZONE)
+
+        # Rename start_date, value, device columns to x, y, legend respectively
+        df.rename(columns={'start_date': 'x', 'value': 'y', 'device': 'legend'}, inplace=True)
+
+        charts[sample_type.label.split(' (')[0]] = chart_service.generate_line_chart(
+            ChartData(
+                title=str(sample_type.label),
+                label_x=gettext('Date'),
+                label_y=str(sample_type.label),
+                label_legend=gettext('Device'),
+                data=df,
+            ),
+        )
+
+    # Build charts for the measurements that contain two values (e.g., blood pressure)
+
+    df = pd.DataFrame(QuantitySample.objects.fetch_blood_pressure_measurements(patient))
+
+    if df.empty:
+        charts[gettext('Blood Pressure')] = None
+    else:
+        # By default the timezone in the dataframe is UTC.
+        # Should be set to the local timezone: https://stackoverflow.com/a/50062101
+        df['measured_at'] = df['measured_at'].dt.tz_convert(settings.TIME_ZONE)
+
+        # Rename start_date, diastolic, systolic, device columns to x, error_max, error_min, legend respectively
+        df.rename(
+            columns={'measured_at': 'x', 'systolic': 'error_max', 'diastolic': 'error_min', 'device': 'legend'},
+            inplace=True,
+        )
+
+        charts[gettext('Blood Pressure')] = chart_service.generate_error_bar_chart(
+            ChartData(
+                title='{0} (mmHg)'.format(gettext('Blood Pressure')),
+                label_x=gettext('Date'),
+                label_y=gettext('Blood Pressure'),
+                label_legend=gettext('Device'),
+                data=df,
+            ),
+        )
+
+    return charts
