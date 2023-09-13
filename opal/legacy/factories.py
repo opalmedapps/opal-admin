b5b5f61baa9fc8abf891f69bfc0f0f8a83b63a51
Stacey Beard <staceybeard10@gmail.com>
2023-09-13 15:31:09 -0400

    fix: fix failing test

--- opal/legacy/factories.py
+++ opal/legacy/factories.py
@@ -15,7 +15,7 @@ class LegacyUserFactory(DjangoModelFactory):
     class Meta:
         model = models.LegacyUsers
 
-    usersernum = 1
+    usersernum = Sequence(lambda number: number + 1)
     usertypesernum = 51
     username = 'username'
     usertype = 'Patient'
