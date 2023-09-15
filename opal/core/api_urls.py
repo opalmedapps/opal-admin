b2ce9c082d7b47aa275a27dc1bdb167905924c56
Matthias Schoettle <git@mattsch.com>
2023-09-15 13:56:59 -0400

    add url tests

--- opal/core/api_urls.py
+++ opal/core/api_urls.py
@@ -96,7 +96,7 @@ urlpatterns = [
     path(
         'institution/',
         settings_views.RetrieveInstitutionView.as_view(),
-        name='institutions-retrieve',
+        name='institution-retrieve',
     ),
     path(
         'institutions/<int:pk>/terms-of-use/',
