69acbf5ca0bc11df39a2641f867e2db97d98e7a4
Shifeng Chen <shifeng.chen@muhc.mcgill.ca>
2023-11-14 02:22:36 +0000

    feat: add delay fields to patient model
    See merge request opalmedapps/opal-admin#862

--- opal/core/models.py
+++ opal/core/models.py
@@ -1,5 +1,6 @@
 """This module provides models for core settings."""
 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -14,6 +15,36 @@ class Address(models.Model):
     province_code = models.CharField(_('Province Code'), default='', max_length=2)
     contact_telephone = models.CharField(_('Contact Telephone'), default='', max_length=100)
     contact_fax = models.CharField(_('Contact Fax'), default='', max_length=100, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractLabDelayModel(models.Model):
+    """Abstract class representing 2 lab result delay fields."""
+
+    non_interpretable_lab_result_delay = models.PositiveIntegerField(
+        verbose_name=_('Non-Interpretable Lab Result Delay'),
+        help_text=_(
+            'Lab result delay for pediatric patients when clinician interpretation is recommended in lab setting.',
+        ),
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(99),
+        ],
+    )
+    interpretable_lab_result_delay = models.PositiveIntegerField(
+        verbose_name=_('Interpretable Lab Result Delay'),
+        help_text=_(
+            'Lab result delay for pediatric patients when clinician interpretation is not specified in lab setting.',
+        ),
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(99),
+        ],
+    )
 
     class Meta:
         abstract = True
