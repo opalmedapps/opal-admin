5d286086c3e0d42efc18a8173b52659473009208
Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
2024-02-29 21:08:55 +0000

    ci: add container scanning
    See merge request opalmedapps/backend!1030

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# option pipefail available on alpine sh
+# shellcheck disable=SC3040
+set -euo pipefail
 
-# Start the cron scheduler service
-service cron start
+# Start the cron scheduler service and log to the container's stdout
+busybox crond -b -l 8 -L /dev/stdout
 
 # execute remaining commands/arguments
 exec "$@"
