fb72a5dacd06dc41f5e7a2b45e33a7a31355d222
Matthias Schoettle <git@mattsch.com>
2024-06-10 14:57:18 +0000

    fix: improve deployment for gunicorn and whitenoise
    See merge request opalmedapps/opal-admin#1183
    
    Merged-by: Matthias Schoettle <git@mattsch.com>
    Approved-by: Kelly Agnew <kelly.agnew@mail.mcgill.ca>
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- docker/start.sh
+++ docker/start.sh
@@ -5,9 +5,11 @@ set -euo pipefail
 
 python /app/manage.py collectstatic --noinput
 
-
+# store temporary file in memory storage
+# https://pythonspeed.com/articles/gunicorn-in-docker/
 /usr/local/bin/gunicorn config.wsgi \
     --workers=4 \
+    --worker-tmp-dir /dev/shm \
     --bind 0.0.0.0:8000  \
     --timeout 90 \
     --chdir=/app
