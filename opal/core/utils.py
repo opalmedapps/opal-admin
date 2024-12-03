8d42f5ff9aefcc4a9bcab2b3bc9138f15abf2c8f
Anton Gladyr <anton.gladyr+opal@gmail.com>
2024-12-03 19:35:09 +0000

    feat: add helper function for converting dictionaries to XLSX
    See merge request opalmedapps/opal-admin#1373
    
    Merged-by: Anton Gladyr <anton.gladyr+opal@gmail.com>
    Approved-by: Matthias Schoettle <git@mattsch.com>
    Approved-by: Kelly Agnew <kelly.agnew@mail.mcgill.ca>

--- opal/core/utils.py
+++ opal/core/utils.py
@@ -2,14 +2,26 @@
 import csv
 import io
 import random
+import re
 import secrets
 import string
 import uuid
 import zipfile
-from typing import Any
+from typing import Any, TypeAlias
+
+from django.utils.text import Truncator
 
 import qrcode
+from openpyxl import Workbook
 from qrcode.image import svg
+
+# Type aliases
+RowData: TypeAlias = dict[str, Any]
+SheetData: TypeAlias = list[RowData]
+WorkbookData: TypeAlias = dict[str, SheetData]
+
+FORBIDDEN_CHARACTERS = r'[\/\\\?\*\:\[\]]'
+SHEET_TITLE_MAX_LENGTH = 31
 
 
 def generate_random_number(length: int) -> str:
@@ -127,3 +139,58 @@ def dict_to_csv(dicts: list[dict[str, Any]]) -> bytes:
     csv_string = buffer.getvalue()
     # Encode the string to bytes
     return csv_string.encode()
+
+
+def dict_to_xlsx(dicts: WorkbookData) -> bytes:
+    """Create an XLSX file from a mapping of dictionaries and return it as bytes.
+
+    Each `RowData` dictionary is expected to have the same keys, as the sheet header is
+    determined by the keys of the first dictionary.
+
+    Note that worksheet names cannot contain more than 31 characters and some symbols are forbidden.
+
+    Please see: https://support.microsoft.com/en-us/office/rename-a-worksheet-3f1f7148-ee83-404d-8ef0-9ff99fbad1f9
+
+    If the worksheet name contains more than 31 characters, it will be truncated.
+    The forbidden symbols in the worksheet name will be removed as well.
+
+    Args:
+        dicts: a mapping where each key is a sheet name, and the value is a list of dictionaries representing rows.
+
+    Returns:
+        bytes: the XLSX file content as bytes.
+    """
+    workbook = Workbook()
+    # Remove the default sheet created by openpyxl if dicts is not empty
+    if dicts:
+        workbook.remove(workbook.active)
+
+    for sheet_name, rows in dicts.items():
+        _add_sheet_to_workbook(workbook, sheet_name, rows)
+
+    output_stream = io.BytesIO()
+    workbook.save(output_stream)
+    return output_stream.getvalue()
+
+
+def _add_sheet_to_workbook(workbook: Workbook, sheet_name: str, rows: SheetData) -> None:
+    """Add a sheet with the given name and rows to the workbook.
+
+    Args:
+        workbook: the workbook to add the sheet to.
+        sheet_name: The name of the sheet.
+        rows: the data rows to add to the sheet.
+    """
+    sheet_name = re.sub(FORBIDDEN_CHARACTERS, '', sheet_name)
+    truncator = Truncator(sheet_name)
+    sheet_name = truncator.chars(num=SHEET_TITLE_MAX_LENGTH)
+    worksheet = workbook.create_sheet(title=sheet_name)
+    # If sheet data is empty, continue to next sheet
+    if not rows:
+        return
+
+    headers = list(rows[0].keys())
+    worksheet.append(headers)
+
+    for row_data in rows:
+        worksheet.append([row_data.get(header, '') for header in headers])
