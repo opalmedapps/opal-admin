c80bdc37a3275261bac2e8ded7e59adcc507418e
Matthias Schoettle <git@mattsch.com>
2024-12-04 09:29:20 -0500

    test: split questionnairedb schema and data

---
+++ opal/tests/sql/questionnairedb_data.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:944c473e32325b40115c2e1fe1f5a20e6ef0c8f99edcb55a2cb1ac79514d1f6d
+size 3795078
---
+++ opal/tests/sql/questionnairedb_schema.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:982e5ae32d36ff5514d4337e5bed2cd7650ed7bf193e344bdc641a554da36bb4
+size 107084
