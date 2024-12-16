f9b4d5cf66c96be73bc41b81936a3233368f10eb
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2024-12-16 19:43:06 +0000

    feat: add form for exporting individual usage statistics
    See merge request opalmedapps/backend!1356
    
    Merged-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
    Approved-by: Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
    Approved-by: Kelly Agnew <kelly.agnew@mail.mcgill.ca>

--- opal/usage_statistics/tests/test_views.py
+++ opal/usage_statistics/tests/test_views.py
@@ -8,7 +8,8 @@ from pytest_django.asserts import assertContains, assertTemplateUsed
 
 # Add any future GET-requestable usage statistics pages here for faster test writing
 test_url_template_data: list[tuple[str, str]] = [
-    (reverse('usage-statistics:data-export'), 'usage_statistics/export_data/export_form.html'),
+    (reverse('usage-statistics:reports-group-export'), 'usage_statistics/export_data/export_form.html'),
+    (reverse('usage-statistics:reports-individual-export'), 'usage_statistics/export_data/export_form.html'),
 ]
 
 
@@ -28,9 +29,20 @@ def test_statistics_views_use_correct_template(admin_client: Client, url: str, t
     assertTemplateUsed(response, template)
 
 
-def test_usage_statistics_export_unauthorized(user_client: Client) -> None:
-    """Ensure that an authenticated (not admin) user cannot access the Export Data page."""
-    response = user_client.get(reverse('usage-statistics:data-export'))
+def test_usage_statistics_group_export_unauthorized(user_client: Client) -> None:
+    """Ensure that an authenticated (not admin) user cannot access the group reports page."""
+    response = user_client.get(reverse('usage-statistics:reports-group-export'))
+
+    assertContains(
+        response=response,
+        text='403 Forbidden',
+        status_code=HTTPStatus.FORBIDDEN,
+    )
+
+
+def test_usage_statistics_individual_export_unauthorized(user_client: Client) -> None:
+    """Ensure that an authenticated (not admin) user cannot access the individual reports page."""
+    response = user_client.get(reverse('usage-statistics:reports-individual-export'))
 
     assertContains(
         response=response,
