101348e0ab51cacab82451dcc57c4e3452fe3baa
Matthias Schoettle <git@mattsch.com>
2024-12-17 15:27:04 -0500

    argh

--- opal/core/signals.py
+++ opal/core/signals.py
@@ -19,5 +19,5 @@ def bind_app_user(request: HttpRequest, logger: logging.Logger, **kwargs: Any) -
         logger: The logger
         kwargs: additional keyword arguments
     """
-    if 'appuserid' in request.POST:
-        structlog.contextvars.bind_contextvars(app_user=request.POST.get('appuserid'))
+    if 'Appuserid' in request.POST:
+        structlog.contextvars.bind_contextvars(app_user=request.POST.get('Appuserid'))
