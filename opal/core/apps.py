d5f42c4313f8547ced538dd2756a8a53ea6b4445
Matthias Schoettle <git@mattsch.com>
2024-12-17 14:51:29 -0500

    register signals when core app loads

--- opal/core/apps.py
+++ opal/core/apps.py
@@ -7,3 +7,9 @@ class CoreConfig(AppConfig):
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'opal.core'
+
+    def ready(self) -> None:
+        """Perform initialization tasks."""
+        # Implicitly connect signal handlers decorated with @receiver.
+        # See: https://docs.djangoproject.com/en/dev/topics/signals/#connecting-receiver-functions
+        from . import signals  # noqa: F401, WPS433
