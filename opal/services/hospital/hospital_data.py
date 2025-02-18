a6861886f9ca8d3fd0725fb74867cc0c0ea62811
Matthias Schoettle <git@mattsch.com>
2025-02-18 17:15:23 -0500

    refactor: remove old hospital integration functionality

--- opal/services/hospital/hospital_data.py
+++ opal/services/hospital/hospital_data.py
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 """Module providing custom data structures (a.k.a., named tuples) for the Source System Service."""
-from datetime import date, datetime
+
+from datetime import datetime
 from typing import NamedTuple
 
 
@@ -24,47 +25,3 @@ class SourceSystemReportExportData(NamedTuple):
     base64_content: str
     document_number: str
     document_date: datetime
-
-
-class SourceSystemMRNData(NamedTuple):
-    """
-    Typed `NamedTuple` that describes MRN fields (a.k.a., MRN data structure) returned from the Source System.
-
-    Attributes:
-        mrn (str): one of the patient's MRNs associated with a particular site
-        site (str): one of the patient's site codes associated with a particular MRN
-        active (bool): MRN is active or not
-    """
-
-    site: str
-    mrn: str
-    active: bool
-
-
-class SourceSystemPatientData(NamedTuple):
-    """
-    Typed `NamedTuple` that describes Patient fields (a.k.a., Patient data structure) returned from the SourceSystem.
-
-    Attributes:
-        date_of_birth: the date of birth
-        first_name: patient first name
-        last_name: patient last name
-        sex: patient sex
-        alias: alias name
-        deceased: True if the patient is deceased, False otherwise
-        death_date_time: patient death date and time
-        ramq: Quebec health insurance number
-        ramq_expiration: the expiration date of the health insurance number
-        mrns: list of MRNs the patient has
-    """
-
-    date_of_birth: date
-    first_name: str
-    last_name: str
-    sex: str
-    alias: str
-    deceased: bool
-    death_date_time: datetime | None
-    ramq: str
-    ramq_expiration: datetime | None
-    mrns: list[SourceSystemMRNData]
--- opal/services/hospital/tests/test_hospital_data.py
+++ opal/services/hospital/tests/test_hospital_data.py
@@ -2,15 +2,10 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from datetime import timedelta
 
 from django.utils import timezone
 
-from dateutil.relativedelta import relativedelta
-
 from opal.services.hospital.hospital_data import (
-    SourceSystemMRNData,
-    SourceSystemPatientData,
     SourceSystemReportExportData,
 )
 
@@ -23,26 +18,4 @@ def test_report_data_type() -> None:
         base64_content='base64',
         document_number='FMU',
         document_date=timezone.now(),
-    )
-
-
-def test_patient_data_type() -> None:
-    """Ensure `SourceSystemPatientData` NamedTuple and `SourceSystemMRNData` NamedTuple can be instantiated."""
-    assert SourceSystemPatientData(
-        date_of_birth=timezone.now().date(),
-        first_name='aaa',
-        last_name='bbb',
-        sex='M',
-        alias='',
-        deceased=True,
-        death_date_time=timezone.now() + relativedelta(years=70),
-        ramq='RAMQ12345678',
-        ramq_expiration=timezone.now() + timedelta(days=100),
-        mrns=[
-            SourceSystemMRNData(
-                site='RVH',
-                mrn='9999996',
-                active=True,
-            ),
-        ],
     )
