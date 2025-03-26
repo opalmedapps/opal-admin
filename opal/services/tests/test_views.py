26f7672bfa8ec3977859b85849541dcddc6f0183
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:40:41 +0000

    feat deps add test functions for the apis
    (cherry picked from commit 297def87e41c44e20b2e31d96be1aec35bfd0f27)
    
    Co-authored-by: Limin <llmgd@hotmail.com>

---
+++ opal/services/tests/test_views.py
@@ -0,0 +1,64 @@
+
+from pytest_mock.plugin import MockerFixture
+
+from .. import hospital
+
+OIE_data = {
+    'status': 'success',
+    'data': {
+        'dateOfBirth': '1953-01-01 00:00:00',
+        'firstName': 'SANDRA',
+        'lastName': 'TESTMUSEMGHPROD',
+        'sex': 'F',
+        'alias': '',
+        'ramq': 'TESS53510111',
+        'ramqExpiration': '2018-01-31 23:59:59',
+        'mrns': [
+            {
+                'site': 'MGH',
+                'mrn': '9999993',
+                'active': True,
+            },
+        ],
+    },
+}
+
+
+def test_find_patient_by_mrn_success(mocker: MockerFixture) -> None:
+    """Ensure that find_patient_by_mrn return the expected OIE data structure."""
+    # mock find_patient_by_mrn and pretend it was unsuccessful
+    mock_oie_response = mocker.patch('opal.services.hospital.find_patient_by_mrn')
+    mock_oie_response.return_value = OIE_data
+
+    response = hospital.find_patient_by_mrn('9999993', 'MGH')
+    assert response == OIE_data
+
+
+def test_find_patient_by_mrn_failure(mocker: MockerFixture) -> None:
+    """Ensure that find_patient_by_mrn return the expected OIE data structure."""
+    # mock find_patient_by_mrn and pretend it was unsuccessful
+    mock_oie_response = mocker.patch('opal.services.hospital.find_patient_by_mrn')
+    mock_oie_response.return_value = None
+
+    response = hospital.find_patient_by_mrn('9999993', 'MGH')
+    assert response != OIE_data
+
+
+def test_find_patient_by_ramq_success(mocker: MockerFixture) -> None:
+    """Ensure that find_patient_by_ramq return the expected OIE data structure."""
+    # mock find_patient_by_mrn and pretend it was unsuccessful
+    mock_oie_response = mocker.patch('opal.services.hospital.find_patient_by_ramq')
+    mock_oie_response.return_value = OIE_data
+
+    response = hospital.find_patient_by_ramq('AAAA9999999')
+    assert response == OIE_data
+
+
+def test_find_patient_by_ramq_failure(mocker: MockerFixture) -> None:
+    """Ensure that find_patient_by_ramq return the expected OIE data structure."""
+    # mock find_patient_by_mrn and pretend it was unsuccessful
+    mock_oie_response = mocker.patch('opal.services.hospital.find_patient_by_ramq')
+    mock_oie_response.return_value = None
+
+    response = hospital.find_patient_by_ramq('AAAA9999999')
+    assert response != OIE_data
