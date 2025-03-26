27b271d6fcef3626ebd17716ec1714c000460363
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 02:22:31 +0000

    fix test
    (cherry picked from commit c5a443da7592a9fcac05dd76015caf7529186ea0)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- opal/patients/tests/test_utils.py
+++ opal/patients/tests/test_utils.py
@@ -499,6 +499,7 @@ def test_initialize_new_opal_patient_orms_success(mocker: MockerFixture) -> None
 @pytest.mark.usefixtures('_set_orms_disabled')
 def test_initialize_new_opal_patient_orms_success_disabled(mocker: MockerFixture) -> None:
     """An info message is logged when the call to ORMS is not executed because it is disabled."""
+    RequestMockerTest.mock_requests_post(mocker, {'status': 'Success'})
     mock_logger = mocker.patch('logging.Logger.info')
 
     rvh_site: hospital_models.Site = Site(acronym='RVH')
