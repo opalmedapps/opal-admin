07b8a505c5a2771d0d9b743dfc2271db12b274b8
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:14:08 +0000

    fix: address code quality issue
    (cherry picked from commit 6ba03e40090444c412853d6cfae2986ca651c0a8)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/services/reports.py
+++ opal/services/reports.py
@@ -86,6 +86,7 @@ class ReportService():
                 url=settings.LEGACY_QUESTIONNAIRES_REPORT_URL,
                 headers=headers,
                 data=payload,
+                timeout=10,
             )
         except RequestException:
             return None
