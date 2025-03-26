77fc6e6f4f6123d53a8cbc9fd8e7049cffef10e9
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:28:25 +0000

    Adds initial model for hospital settings.
    See: #8
    
    
    (cherry picked from commit 3a5d4e04d1937755ea25b672a9729197c5013138)
    
    Co-authored-by: Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>

--- opal/hospital_settings/models.py
+++ opal/hospital_settings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Location(models.Model):
+    class Meta:
+        abstract = True
+
+    name = models.CharField(_('Name'), max_length=100)
+    code = models.CharField(_('Code'), max_length=10)
+    parking_url = models.URLField(_('Parking Info'))
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Institution(Location):
+    pass
+
+
+class Site(Location):
+    institution = models.ForeignKey(
+        to=Institution,
+        on_delete=models.CASCADE,
+        related_name='sites',
+        verbose_name=_('Institution'),
+    )
