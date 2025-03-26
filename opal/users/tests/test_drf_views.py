91c5c3ed3cc2e4465a3f12adcd33194a639c9c6e
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:47:13 +0000

    feat: test permissions
    (cherry picked from commit 7965d6b22a34b8f64ab859907f96a2e00f766c79)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/users/tests/test_drf_views.py
+++ opal/users/tests/test_drf_views.py
@@ -15,7 +15,7 @@ pytestmark = pytest.mark.django_db
 def test_groups_list_pass(api_client: APIClient, admin_user: User) -> None:
     """Test the pass of the retrieving list of groups."""
     api_client.force_login(user=admin_user)
-    user_factories.GroupFactory(name='group1')
+    group = user_factories.GroupFactory(name='group1')
     user_factories.GroupFactory(name='group2')
 
     response = api_client.get(reverse(
@@ -23,3 +23,17 @@ def test_groups_list_pass(api_client: APIClient, admin_user: User) -> None:
     ))
     assert response.status_code == HTTPStatus.OK
     assert len(response.data) == 2
+    assert response.data[0]['pk'] == group.pk
+
+
+def test_groups_list_fail(api_client: APIClient, django_user_model: User) -> None:
+    """Test the failure of the retrieving list of groups due to wrong permissions."""
+    user = django_user_model.objects.create(username='test_user')
+    api_client.force_login(user=user)
+    user_factories.GroupFactory(name='group1')
+
+    response = api_client.get(reverse(
+        'api:groups-list',
+    ))
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.data['detail'] == 'You do not have permission to perform this action.'
