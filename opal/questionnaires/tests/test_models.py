b15d24e2116213a342839f0a7c1ba2c96921a0bd
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:57:00 +0000

    test: add test for model update method
    (cherry picked from commit 9c1f2ed135ca5f71d5d995db1624ca0bbed84b54)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- opal/questionnaires/tests/test_models.py
+++ opal/questionnaires/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .. import factories
+from .. import factories, models
 
 pytestmark = pytest.mark.django_db
 
@@ -16,3 +16,34 @@ def test_questionnaireprofile_str() -> None:
     questionnaire_profile = factories.QuestionnaireProfile()
     expected_follows_string = "{'19': {'title': 'Opal Feedback Questionnaire', 'lastviewed': '2022-11-17'}}"
     assert str(questionnaire_profile).split('__')[2] == expected_follows_string
+
+
+def test_questionnaireprofile_update() -> None:
+    """Ensure QuestionnaireProfile factory builds properly."""
+    questionnaire_profile = factories.QuestionnaireProfile()
+    models.QuestionnaireProfile.update_questionnaires_following(
+        qid='13',
+        qname='Test Qst Add',
+        user=questionnaire_profile.user,
+        toggle=True,
+    )
+    retrieve_profile, created = models.QuestionnaireProfile.objects.get_or_create(user=questionnaire_profile.user)
+
+    assert '13' in retrieve_profile.questionnaire_list
+    assert len(retrieve_profile.questionnaire_list) == 2
+    assert not created  # Should not need to create a new profile
+
+
+def test_questionnaireprofile_toggle_off() -> None:
+    """Ensure QuestionnaireProfile factory builds properly."""
+    questionnaire_profile = factories.QuestionnaireProfile()
+    models.QuestionnaireProfile.update_questionnaires_following(
+        qid='19',
+        qname='Test Qst Add',
+        user=questionnaire_profile.user,
+        toggle=False,
+    )
+    retrieve_profile, created = models.QuestionnaireProfile.objects.get_or_create(user=questionnaire_profile.user)
+
+    assert not retrieve_profile.questionnaire_list
+    assert not created
