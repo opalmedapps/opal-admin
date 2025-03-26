368499a2979809f189b84c95062db3baeb651a1e
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:45:53 +0000

    feat deps refactor the code
    (cherry picked from commit 188e2f2544b317e67347bee5200d48dfb3d2df53)
    
    Co-authored-by: lymanliugd <Limin.Liu@muhc.mcgill.ca>

--- opal/legacy/management/commands/migrate_securityanswers.py
+++ opal/legacy/management/commands/migrate_securityanswers.py
@@ -1,10 +1,11 @@
 """Command for Security Answer migration."""
 from typing import Any, Optional
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from opal.caregivers.models import CaregiverProfile, SecurityAnswer, SecurityQuestion
-from opal.legacy.models import LegacySecurityAnswer, LegacySecurityQuestion, LegacyUsers
+from opal.caregivers.models import CaregiverProfile, SecurityAnswer
+from opal.legacy.models import LegacySecurityAnswer, LegacyUsers
 from opal.users.models import User
 
 
@@ -37,17 +38,6 @@ class Command(BaseCommand):
                 )
                 continue
 
-            legacy_question = legacy_answer.securityquestionsernum
-            question = self._find_question(legacy_question.securityquestionsernum)
-            if question is None:
-                self.stderr.write(
-                    'Security answer import failed, sernum: {answersernum}, details: {details}'.format(
-                        answersernum=legacy_answer.securityanswersernum,
-                        details='Question does not exist',
-                    ),
-                )
-                continue
-
             # Check caregiver according to the user, skip import if not found.
             try:
                 caregiver = CaregiverProfile.objects.get(user_id=user.id)
@@ -60,17 +50,22 @@ class Command(BaseCommand):
                 )
                 continue
 
+            legacy_question = legacy_answer.securityquestionsernum
+            question_text = legacy_question.questiontext_en  # English
+            if user.language == settings.LANGUAGES[1][0]:  # French
+                question_text = legacy_question.questiontext_fr
+
             # Import SecurityAnwser or create if it does not exist.
             try:
                 SecurityAnswer.objects.get(
                     user_id=caregiver.id,
-                    question=question.title,
+                    question=question_text,
                     answer=legacy_answer.answertext,
                 )
             except SecurityAnswer.DoesNotExist:
                 SecurityAnswer.objects.create(
                     user=caregiver,
-                    question=question.title,
+                    question=question_text,
                     answer=legacy_answer.answertext,
                 )
                 self.stdout.write(
@@ -78,39 +73,6 @@ class Command(BaseCommand):
                         answersernum=legacy_answer.securityanswersernum,
                     ),
                 )
-
-    def _find_question(self, securityquestionsernum: int) -> Any:
-        """
-        Check legacy security question exists or not.
-
-        Args:
-            securityquestionsernum: legacy security question sernum.
-
-        Returns:
-            Return question if found otherwise return None.
-        """
-        # Skip answer import if related legacy question not found
-        try:
-            legacy_question = LegacySecurityQuestion.objects.get(
-                securityquestionsernum=securityquestionsernum,
-            )
-        except LegacySecurityQuestion.DoesNotExist:
-            self.stderr.write(
-                'Legacy security question does not exist, sernum: {questionsernum}'.format(
-                    questionsernum=securityquestionsernum,
-                ),
-            )
-        if legacy_question:
-            # Import SecurityQuestion according to legacy security question or create if not exists.
-            try:
-                question = SecurityQuestion.objects.get(title_en=legacy_question.questiontext_en)
-            except SecurityQuestion.DoesNotExist:
-                self.stderr.write(
-                    'Security question does not exist, title_en: {title_en}'.format(
-                        title_en=legacy_question.questiontext_en,
-                    ),
-                )
-        return question
 
     def _find_user(self, patientsernum: int) -> Optional[User]:  # noqa: C901
         """
