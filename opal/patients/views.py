67632bcd84ecbf3cfa6e2ccd08754dae0bc7289f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:34:17 +0000

    fix refactor the code
    (cherry picked from commit 2212b67754d85f1c9b93bd1ba4b712950d985226)
    
    Co-authored-by: LiminLiu <Limin.Liu@muhc.mcgill.ca>

--- opal/patients/views.py
+++ opal/patients/views.py
@@ -273,10 +273,9 @@ class NewAccessRequestView(  # noqa: WPS214, WPS215 (too many methods, too many 
         template_name = self.template_name_confirmation
 
         if registration_code:
-            code_url = f'{settings.OPAL_USER_REGISTRATION_URL}/#!code={registration_code.code}'
-            web_code_url = f'{settings.OPAL_USER_REGISTRATION_URL}/#!/form/search?code={registration_code.code}'
+            code_url = f'{settings.OPAL_USER_REGISTRATION_URL}/#!/form/search?code={registration_code.code}'
             context.update({
-                'registration_url': web_code_url,
+                'registration_url': settings.OPAL_USER_REGISTRATION_URL,
                 'registration_code': registration_code.code,
                 'qr_code': base64.b64encode(qr_code(code_url)).decode(),
             })
