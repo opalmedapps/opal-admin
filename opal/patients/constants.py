0c7496aeb33fd7c8aae32f47ef8e264f200b227f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:54:43 +0000

    fix: add TODO comment following the code review
    (cherry picked from commit 67716f3be7fc737ade718506c98752d375e1966a)
    
    Co-authored-by: Shifeng Chen <shifeng.chen@muhc.mcgill.ca>

--- opal/patients/constants.py
+++ opal/patients/constants.py
@@ -10,4 +10,5 @@ RELATIONSHIP_MIN_AGE: Final = 0
 #: Choices for the type of the medical cards
 MEDICAL_CARDS: Final = (('mrn', _('Medical Record Number (MRN)')), ('ramq', _('Medicare Card Number (RAMQ)')))
 #: Choices for the type of users
+# TODO: we might refactor this constant name for more clarity
 TYPE_USERS: Final = ((0, _('New Opal User')), (1, _('Existing Opal User')))
