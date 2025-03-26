5ea0db9727e2e6a626d6f907ab35c04ee54e8fab
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:44:00 +0000

    ...
    (cherry picked from commit 104aeb2bb6585dbf88b1dcde1a01565efa29824f)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- opal/hospital_settings/tests/test_drf_viewsets.py
+++ opal/hospital_settings/tests/test_drf_viewsets.py
@@ -23,8 +23,8 @@ def test_api_institutions_list(api_client: APIClient, admin_user: User) -> None:
     response = api_client.get(reverse('api:institutions-list'))
 
     assert response.status_code == HTTPStatus.OK
-    assert response.data['count'] == 1
-    assert response.data['results'][0]['id'] == institution.pk
+    assert len(response.data) == 1
+    assert response.data[0]['id'] == institution.pk
 
 
 def test_api_institutions_list_allowed_methods(api_client: APIClient, admin_user: User) -> None:
@@ -87,8 +87,8 @@ def test_api_site_list(api_client: APIClient, admin_user: User) -> None:
     response = api_client.get(reverse('api:sites-list'))
 
     assert response.status_code == HTTPStatus.OK
-    assert response.data['count'] == 1
-    assert response.data['results'][0]['id'] == site.pk
+    assert len(response.data) == 1
+    assert response.data[0]['id'] == site.pk
 
 
 def test_api_site_retrieve(api_client: APIClient, admin_user: User) -> None:
