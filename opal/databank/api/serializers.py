981d6ee76162f447b6ce6364ad1e98aee5f733c2
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:50:50 +0000

    Merge branch 'main' into QSCCD-1443.AG.update_patient_deviations_command
    (cherry picked from commit f343bee5f48f885a740a546789e70537c02e1194)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/databank/api/serializers.py
+++ opal/databank/api/serializers.py
@@ -1,4 +1,6 @@
 """Serializers for the API views of the `databank` app."""
+from rest_framework import serializers
+
 from opal.core.api.serializers import DynamicFieldsSerializer
 
 from ..models import DatabankConsent
@@ -6,6 +8,10 @@ from ..models import DatabankConsent
 
 class DatabankConsentSerializer(DynamicFieldsSerializer):
     """Serializer for DatabankConsents."""
+
+    # Non model fields used for GUID generation
+    middle_name = serializers.CharField(required=True, allow_blank=True, write_only=True)
+    city_of_birth = serializers.CharField(required=True, write_only=True)
 
     class Meta:
         model = DatabankConsent
@@ -15,4 +21,6 @@ class DatabankConsentSerializer(DynamicFieldsSerializer):
             'has_demographics',
             'has_labs',
             'has_questionnaires',
+            'middle_name',
+            'city_of_birth',
         ]
