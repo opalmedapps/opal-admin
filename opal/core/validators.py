bb4cd42226c03109286f58275d940a69f43ba89e
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:42:47 +0000

    refactor: update validator according to comments
    (cherry picked from commit e367ceb6af9db2471d22cd958d3026d5afe4cd18)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/core/validators.py
+++ opal/core/validators.py
@@ -1,8 +1,8 @@
 """Module for common validators of specific fields throughout opal system."""
 from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
-# this ramq validator is an instance of RegexValidator. to be improved later.
-ramq_validator = RegexValidator(
-    regex='^[a-zA-Z]{4}[0-9]{8}$',
-    message='RAMQ format is: LLLLDDDDDDDD - 4 Letters followed by 8 Digits',
+validate_ramq = RegexValidator(
+    regex='^[A-Z]{4}[0-9]{8}$',
+    message=_('Enter a valid RAMQ number consisting of 4 letters followed by 8 digits'),
 )
