7837c06353450bbc1a4c719fd2979ac48d1207c5
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:30:15 +0000

    feat: Add custom user model
    (cherry picked from commit a913ea980feb52694e3d0ca96ee2ea7fc29b03f6)
    
    Co-authored-by: Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>

---
+++ opal/users/models.py
@@ -0,0 +1,13 @@
+"""
+Module providing models for the users app.
+
+Provides a custom user model based on Django's [django.contrib.auth.models.AbstractUser][].
+For more information see the [Django documentation on customizing the user model](https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project)
+"""  # noqa: E501
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """Default custom user model."""
+
+    pass  # noqa: WPS420, WPS604
