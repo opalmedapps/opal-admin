cfe5c76b3f5471ac3a952262de5da3f845ebfc49
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:42:45 +0000

    fix flakes compaints about the validator
    (cherry picked from commit 24869436c3448ccde9314b6bb7610ebb36ecf378)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/core/validators.py
+++ opal/core/validators.py
@@ -1,7 +1,8 @@
 """Module for common validators of specific fields throughout opal system."""
 from django.core.validators import RegexValidator
 
-
-#: this ramq validator is an instance of RegexValidator. to be improved later.
-ramq_validator = RegexValidator(r'^[a-zA-Z]{4}[0-9]{8}$', 'RAMQ should be 4 letters followed by 8 digits')
-
+# this ramq validator is an instance of RegexValidator. to be improved later.
+ramq_validator = RegexValidator(
+    regex='^[a-zA-Z]{4}[0-9]{8}$',
+    message='RAMQ format is: LLLLDDDDDDDD - 4 Letters followed by 8 Digits',
+)
