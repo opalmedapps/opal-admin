011084d17647549680fe3ef5479d19a3edef752d
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:17:16 +0000

    test: add test for demographic update with deceased patient
    (cherry picked from commit c3075e47288ec6582fabcdd60e0a1a1ccccc8c72)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/patients/api/tests/test_api_views.py
+++ opal/patients/api/tests/test_api_views.py
@@ -18,8 +18,8 @@ from rest_framework.test import APIClient
 from opal.caregivers.factories import CaregiverProfile, Device, RegistrationCode
 from opal.caregivers.models import RegistrationCodeStatus, SecurityAnswer
 from opal.hospital_settings.factories import Institution, Site
+from opal.patients import models as patient_models
 from opal.patients.factories import HospitalPatient, Patient, Relationship
-from opal.patients.models import RelationshipType
 from opal.users.factories import User
 
 pytestmark = pytest.mark.django_db(databases=['default'])
@@ -37,7 +37,7 @@ def test_my_caregiver_list(api_client: APIClient, admin_user: AbstractUser) -> N
         caregiver=caregiver2,
         status='CON',
         # Pytest insists on fetching the SELF role type instance using a queryset for some reason, factory doesnt work
-        type=RelationshipType.objects.self_type(),
+        type=patient_models.RelationshipType.objects.self_type(),
     )
 
     api_client.credentials(HTTP_APPUSERID=caregiver2.user.username)
@@ -557,20 +557,18 @@ class TestPatientDemographicView:
         api_client: APIClient,
     ) -> None:
         """Ensure the endpoint raises a NotFound exception if MRNs referring to different patients."""
-        rvh_site = Site(code='RVH')
-        mgh_site = Site(code='MGH')
         patient_one = Patient()
         patient_two = Patient(ramq='TEST01161972')
 
         HospitalPatient(
             patient=patient_one,
             mrn='9999996',
-            site=rvh_site,
+            site=Site(code='RVH'),
         )
         HospitalPatient(
             patient=patient_two,
             mrn='9999997',
-            site=mgh_site,
+            site=Site(code='MGH'),
         )
 
         client = self.get_client_with_permissions(api_client)
@@ -612,24 +610,22 @@ class TestPatientDemographicView:
         api_client: APIClient,
     ) -> None:
         """Ensure the endpoint can update patient info with no errors."""
-        rvh_site = Site(code='RVH')
-        mgh_site = Site(code='MGH')
         patient = Patient(ramq='TEST01161972')
 
         Relationship(
             patient=patient,
-            type=RelationshipType.objects.self_type(),
+            type=patient_models.RelationshipType.objects.self_type(),
         )
 
         HospitalPatient(
             patient=patient,
             mrn='9999996',
-            site=rvh_site,
+            site=Site(code='RVH'),
         )
         HospitalPatient(
             patient=patient,
             mrn='9999997',
-            site=mgh_site,
+            site=Site(code='MGH'),
         )
 
         client = self.get_client_with_permissions(api_client)
@@ -705,6 +701,57 @@ class TestPatientDemographicView:
             raw=json.dumps(response.json()),
             expected_data=self.get_valid_input_data(),
         )
+
+    def test_demographic_update_deceased_patient(
+        self,
+        api_client: APIClient,
+    ) -> None:
+        """Ensure the endpoint prevents caregiver and self access to the deceased patient's data."""
+        patient = Patient(ramq='TEST01161972')
+
+        Relationship(
+            patient=patient,
+            type=patient_models.RelationshipType.objects.self_type(),
+        ).save()
+        Relationship(
+            patient=patient,
+            caregiver=CaregiverProfile(),
+            type=patient_models.RelationshipType.objects.guardian_caregiver(),
+        ).save()
+
+        HospitalPatient(
+            patient=patient,
+            mrn='9999996',
+            site=Site(code='RVH'),
+        )
+        HospitalPatient(
+            patient=patient,
+            mrn='9999997',
+            site=Site(code='MGH'),
+        )
+
+        client = self.get_client_with_permissions(api_client)
+        payload = self.get_valid_input_data()
+        payload['date_of_death'] = datetime.now().replace(
+            microsecond=0,
+        ).astimezone().isoformat()
+
+        response = client.put(
+            reverse('api:patient-demographic-update'),
+            data=payload,
+            format='json',
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        assertJSONEqual(
+            raw=json.dumps(response.json()),
+            expected_data=payload,
+        )
+
+        relationships = patient_models.Relationship.objects.all()
+        assert relationships[0].status == patient_models.RelationshipStatus.EXPIRED
+        assert relationships[1].status == patient_models.RelationshipStatus.EXPIRED
 
 
 class TestPatientCaregiversView:
