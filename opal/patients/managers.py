4cd7045f533d3c00e7f299cb3f9181941ee61772
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:55:29 +0000

    Adjustments following code review
    (cherry picked from commit 9c9d14bfff230609b03511811e81f02395052c1c)
    
    Co-authored-by: david.gagne <david.gagne@muhc.mcgill.ca>

--- opal/patients/managers.py
+++ opal/patients/managers.py
@@ -6,12 +6,12 @@ from django.db import models
 class RelationshipManager(models.Manager):
     """Manager class for the `Relationship` model."""
 
-    def get_patient_list_for_caregiver(self, user_id: str) -> models.QuerySet:
+    def get_patient_list_for_caregiver(self, user_name: str) -> models.QuerySet:
         """
         Query manager to get a list of patients for a given caregiver.
 
         Args:
-            user_id: User id making the request
+            user_name: User id making the request
 
         Returns:
             Queryset to get the list of patients
@@ -22,20 +22,20 @@ class RelationshipManager(models.Manager):
             'caregiver',
             'caregiver__user',
         ).filter(
-            caregiver__user__username=user_id,
+            caregiver__user__username=user_name,
         )
 
-    def get_patient_id_list_for_caregiver(self, user_id: str) -> list[int]:
+    def get_patient_id_list_for_caregiver(self, user_name: str) -> list[int]:
         """
         Get a array of patients legacy IDs for a given caregiver.
 
         Args:
-            user_id: User id making the request
+            user_name: User id making the request
 
         Returns:
             Return list of patient legacy IDs
         """
-        relationships = self.get_patient_list_for_caregiver(user_id=user_id)
+        relationships = self.get_patient_list_for_caregiver(user_name=user_name)
         return list(relationships.values_list('patient__legacy_id', flat=True))
 
 
