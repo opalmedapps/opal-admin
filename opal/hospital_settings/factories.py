0d5d47b064462f4663616eea20746b4a140f67ae
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:32:39 +0000

    feat: add the factory for Patient model
    	- deal with the case with many-to-many realtion with a 'through'
    
    
    (cherry picked from commit 04dbdecf0ab0cd8c0926b961b3bfbeafb4a0a3c0)
    
    Co-authored-by: LAPTOP-81BFMD7I\shifeng <shifeng.chen@muhc.mcgill.ca>

--- opal/hospital_settings/factories.py
+++ opal/hospital_settings/factories.py
@@ -1,7 +1,33 @@
-"""Module providing model factories for hospital settings app models."""
+"""Module providing model factories for patients app models.
+
+reference from:
+
+  https://factoryboy.readthedocs.io/en/stable/recipes.html#dependent-objects-foreignkey
+
+  Article about "Many-to-many relation with a ‘through’"
+"""
+
+import datetime
+
+from django.utils.translation import gettext_lazy as _
+
+from factory import Faker, LazyFunction, RelatedFactory, Sequence, SubFactory
 from factory.django import DjangoModelFactory
 
+from opal.caregivers.factories import CaregiverProfile
+
 from . import models
+
+
+def get_relationship_status() -> str:
+    """
+    Return a random relationship status from available choices.
+
+    Returns:
+        the random choice from models.RelationshipStatus
+    """
+    relationship_status = [status[0] for status in models.RelationshipStatus]
+    return relationship_status[0]
 
 
 class RelationshipType(DjangoModelFactory):
@@ -16,3 +42,39 @@ class RelationshipType(DjangoModelFactory):
     description_fr = 'Le patient'
     start_age = 14
     form_required = False
+
+
+class Patient(DjangoModelFactory):
+    """Model factory to create [opal.patients.models.Patient][] models."""
+
+    class Meta:
+        model = models.Patient
+
+    first_name = Faker(_('first_name'))
+    last_name = Faker(_('last_name'))
+    day_of_birth = Faker('date_object')
+    legacy_id = Sequence(lambda number: number + 1)
+
+
+class Relationship(DjangoModelFactory):
+    """Model factory to create [opal.patients.models.Relationship][] models."""
+
+    class Meta:
+        model = models.Relationship
+
+    patient = SubFactory(Patient)
+    caregiver = SubFactory(CaregiverProfile)
+    relationship_type = SubFactory(RelationshipType)
+    status = LazyFunction(get_relationship_status)
+    request_date = datetime.date.today()
+    start_date = Faker('date_object')
+    end_date = Faker('date_object')
+
+
+class CaregiverWithPatients(CaregiverProfile):
+    """Model factory to create caregiverWithPatients models."""
+
+    caregivers = RelatedFactory(
+        Relationship,
+        factory_related_name='caregiver',
+    )
