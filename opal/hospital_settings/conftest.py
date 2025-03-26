9058fea4a949e460c8e4a4f951b1a925051da5b7
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:29:40 +0000

    Ignore Institution type error in the conftest.py
    (cherry picked from commit 13ecbee9f825a03a43cd6d76748cc428f149a93c)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@gmail.com>

--- opal/hospital_settings/conftest.py
+++ opal/hospital_settings/conftest.py
@@ -1,17 +1,19 @@
 """This module is used to provide configuration, fixtures, and plugins for pytest within hospital-settings app."""
 
-from typing import Any
-
 import pytest
 
 from .models import Institution
 
 
 @pytest.fixture()
-def institution() -> Any:
+def institution() -> Institution:
     """Fixture providing an instance of ``Institution`` model.
 
     Returns:
         an instance of ``Institution``
     """
-    return Institution.objects.create(name_en='TEST1_EN', name_fr='TEST1_FR', code='TEST1')
+    return Institution.objects.create(  # type: ignore[no-any-return]
+        name_en='TEST1_EN',
+        name_fr='TEST1_FR',
+        code='TEST1',
+    )
