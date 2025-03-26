9e9818bb58c6002a16f76fb199e81b57dce3565d
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:49:00 +0000

    Merge branch 'main' of gitlab.com:opalmedapps/backend into QSCCD-1500_subticket_1599_signal_admin_group
    (cherry picked from commit 5aac049460aac6b85a123499ab0773fc31513e53)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/users/tests/test_drf_urls.py
+++ opal/users/tests/test_drf_urls.py
@@ -3,6 +3,8 @@ from django.urls import resolve, reverse
 
 import pytest
 
+from opal.users import factories
+
 pytestmark = pytest.mark.django_db
 
 
@@ -10,3 +12,44 @@ def test_groups_list() -> None:
     """Ensure groups list is defined."""
     assert reverse('api:groups-list') == '/api/groups/'
     assert resolve('/api/groups/').view_name == 'api:groups-list'
+
+
+def test_users_list() -> None:
+    """Ensure users list is defined."""
+    assert reverse('api:users-list') == '/api/users/'
+    assert resolve('/api/users/').view_name == 'api:users-list'
+
+
+def test_users_detail() -> None:
+    """Ensure users detail is defined."""
+    user = factories.User()
+
+    assert reverse(
+        'api:users-detail',
+        kwargs={'username': user.username},
+    ) == f'/api/users/{user.username}/'  # noqa: WPS221
+    assert resolve(f'/api/users/{user.username}/').view_name == 'api:users-detail'
+
+
+def test_users_action_set_manager_user() -> None:
+    """Ensure users action for `set-manager-user` is defined."""
+    user = factories.User()
+
+    assert reverse(
+        'api:users-set-manager-user',
+        kwargs={'username': user.username},
+    ) == f'/api/users/{user.username}/set-manager-user/'
+
+    assert resolve(f'/api/users/{user.username}/set-manager-user/').view_name == 'api:users-set-manager-user'
+
+
+def test_users_action_unset_manager_user() -> None:
+    """Ensure users action for `unset-manager-user` is defined."""
+    user = factories.User()
+
+    assert reverse(
+        'api:users-unset-manager-user',
+        kwargs={'username': user.username},
+    ) == f'/api/users/{user.username}/unset-manager-user/'
+
+    assert resolve(f'/api/users/{user.username}/unset-manager-user/').view_name == 'api:users-unset-manager-user'
