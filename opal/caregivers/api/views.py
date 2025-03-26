b4b7be44f6cd32ef72670c0d78b5ade187cf4111
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:52:35 +0000

    feat deps separate the api to 2, one for each url
    (cherry picked from commit 4d197aa9d68e39e059f6550e6d1ee2cda5e8211b)
    
    Co-authored-by: lymanliugd <Limin.Liu@muhc.mcgill.ca>

--- opal/caregivers/api/views.py
+++ opal/caregivers/api/views.py
@@ -64,12 +64,84 @@ class GetCaregiverPatientsList(APIView):
         return response
 
 
-class ApiEmailVerificationView(APIView):
-    """Class to save and verify the user's email and email verification code."""
+class ApiVerifyEmailView(APIView):
+    """Class to save the user's email and email verification code.
+
+    And send email to the user with the verification code.
+    """
 
     permission_classes = [IsAuthenticated]
     min_number = 100000
     max_number = 999999
+
+    def get_queryset(self) -> QuerySet[RegistrationCode]:
+        """
+        Override get_queryset to filter RegistrationCode by registration code.
+
+        Returns:
+            The queryset of RegistrationCode
+        """
+        code = self.kwargs.get('code') if hasattr(self, 'kwargs') else None
+        return RegistrationCode.objects.select_related(
+            'relationship__caregiver',
+        ).prefetch_related(
+            'relationship__caregiver__email_verifications',
+        ).filter(code=code, status=RegistrationCodeStatus.NEW)
+
+    def post(self, request: Request, code: str) -> Response:  # noqa: WPS210
+        """
+        Handle POST requests from `registration/<str:code>/verify-email/`.
+
+        Args:
+            request: Http request made by the listener.
+            code: registration code.
+
+        Raises:
+            ValidationError: The object not found.
+
+        Returns:
+            Http response with empty message.
+        """
+        registration_code = None
+        try:
+            registration_code = self.get_queryset().get()
+        except RegistrationCode.DoesNotExist:
+            raise drf_serializers.ValidationError({'detail': _('Registration code is invalid.')})
+
+        input_serializer = EmailVerificationSerializer(data=request.data, fields=('email',), partial=True)
+        input_serializer.is_valid(raise_exception=True)
+
+        email = input_serializer.validated_data['email']
+        verification_code = random.randint(self.min_number, self.max_number)  # noqa: S311
+        caregiver = registration_code.relationship.caregiver
+        try:
+            email_verification = registration_code.relationship.caregiver.email_verifications.get(
+                email=email,
+            )
+        except EmailVerification.DoesNotExist:
+            email_verification = EmailVerification.objects.create(
+                caregiver=caregiver,
+                code=verification_code,
+                email=email,
+                sent_at=timezone.now(),
+            )
+        else:
+            input_serializer.update(
+                email_verification,
+                {
+                    'code': verification_code,
+                    'is_verified': False,
+                    'sent_at': timezone.now(),
+                },
+            )
+
+        return Response()
+
+
+class ApiVerifyEmailCodeView(APIView):
+    """Class to verify the user's email with received verification code."""
+
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self) -> QuerySet[RegistrationCode]:
         """
@@ -121,54 +193,5 @@ class ApiEmailVerificationView(APIView):
             user = registration_code.relationship.caregiver.user
             user.email = email_verification.email
             user.save()
-
-        return Response()
-
-    def post(self, request: Request, code: str) -> Response:  # noqa: WPS210
-        """
-        Handle POST requests from `registration/<str:code>/verify-email/`.
-
-        Args:
-            request: Http request made by the listener.
-            code: registration code.
-
-        Raises:
-            ValidationError: The object not found.
-
-        Returns:
-            Http response with empty message.
-        """
-        registration_code = None
-        try:
-            registration_code = self.get_queryset().get()
-        except RegistrationCode.DoesNotExist:
-            raise drf_serializers.ValidationError({'detail': _('Registration code is invalid.')})
-
-        input_serializer = EmailVerificationSerializer(data=request.data, fields=('email',), partial=True)
-        input_serializer.is_valid(raise_exception=True)
-
-        email = input_serializer.validated_data['email']
-        verification_code = random.randint(self.min_number, self.max_number)  # noqa: S311
-        caregiver = registration_code.relationship.caregiver
-        try:
-            email_verification = registration_code.relationship.caregiver.email_verifications.get(
-                email=email,
-            )
-        except EmailVerification.DoesNotExist:
-            email_verification = EmailVerification.objects.create(
-                caregiver=caregiver,
-                code=verification_code,
-                email=email,
-                sent_at=timezone.now(),
-            )
-        else:
-            input_serializer.update(
-                email_verification,
-                {
-                    'code': verification_code,
-                    'is_verified': False,
-                    'sent_at': timezone.now(),
-                },
-            )
 
         return Response()
