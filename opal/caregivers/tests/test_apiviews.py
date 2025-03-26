193659538a173ca51a2b51818c481c463ad845d7
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:53:25 +0000

    fix deps fix the test function
    (cherry picked from commit fbc36258412201287392762af77c621accef3930)
    
    Co-authored-by: lymanliugd <Limin.Liu@muhc.mcgill.ca>

--- opal/caregivers/tests/test_apiviews.py
+++ opal/caregivers/tests/test_apiviews.py
@@ -170,7 +170,6 @@ class TestApiEmailVerification:
         assert len(mail.outbox) == 1
         assert mail.outbox[0].from_email == settings.EMAIL_HOST_USER
         assert email_verification.code in mail.outbox[0].body
-        assert 'Cher' in mail.outbox[0].body
 
     def test_resend_verify_email_within_ten_sec(self, api_client: APIClient, admin_user: AbstractUser) -> None:
         """Test resend verify email within 10 sec."""
