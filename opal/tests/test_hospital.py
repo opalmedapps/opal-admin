337eb1da5f7d61260a421b911663559e88842b66
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:40:56 +0000

    feat deps fix test functions
    (cherry picked from commit bda6861897c55974826cfc69c468ab9d69b60e2c)
    
    Co-authored-by: Limin <llmgd@hotmail.com>

--- opal/tests/test_hospital.py
+++ opal/tests/test_hospital.py
@@ -24,6 +24,7 @@ OIE_data = {
         ],
     },
 }
+communicate_service = OIECommunicationService()
 
 
 def test_find_patient_by_mrn_success(mocker: MockerFixture) -> None:
@@ -32,7 +33,7 @@ def test_find_patient_by_mrn_success(mocker: MockerFixture) -> None:
     mock_oie_response = mocker.patch('opal.services.hospital.OIECommunicationService.find_patient_by_mrn')
     mock_oie_response.return_value = OIE_data
 
-    response = OIECommunicationService.find_patient_by_mrn('9999993', 'MGH')
+    response = communicate_service.find_patient_by_mrn('9999993', 'MGH')
     data = cast(Dict[str, dict], response)['data']
     assert data is not None
 
@@ -43,7 +44,7 @@ def test_find_patient_by_mrn_failure(mocker: MockerFixture) -> None:
     mock_oie_response = mocker.patch('opal.services.hospital.OIECommunicationService.find_patient_by_mrn')
     mock_oie_response.return_value = None
 
-    response = OIECommunicationService.find_patient_by_mrn('9999993', 'MGH')
+    response = communicate_service.find_patient_by_mrn('9999993', 'MGH')
     assert response is None
 
 
@@ -53,7 +54,7 @@ def test_find_patient_by_ramq_success(mocker: MockerFixture) -> None:
     mock_oie_response = mocker.patch('opal.services.hospital.OIECommunicationService.find_patient_by_ramq')
     mock_oie_response.return_value = OIE_data
 
-    response = OIECommunicationService.find_patient_by_ramq('AAAA9999999')
+    response = communicate_service.find_patient_by_ramq('AAAA9999999')
     data = cast(Dict[str, dict], response)['data']
     assert data is not None
 
@@ -64,5 +65,5 @@ def test_find_patient_by_ramq_failure(mocker: MockerFixture) -> None:
     mock_oie_response = mocker.patch('opal.services.hospital.OIECommunicationService.find_patient_by_ramq')
     mock_oie_response.return_value = None
 
-    response = OIECommunicationService.find_patient_by_ramq('AAAA9999999')
+    response = communicate_service.find_patient_by_ramq('AAAA9999999')
     assert response is None
