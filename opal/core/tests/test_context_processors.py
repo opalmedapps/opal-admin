a6000be650b4e8f64f4878b6c36c5fc056539e1c
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:13:55 +0000

    feat: add context processor to provide the current app's verbose_name
    (cherry picked from commit bb0ef566e82b8df04108b51f304e216d4ba94e6e)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- opal/core/tests/test_context_processors.py
+++ opal/core/tests/test_context_processors.py
@@ -1,8 +1,11 @@
 from django.http import HttpRequest
+from django.test.client import Client
+from django.urls import reverse
 
+import pytest
 from pytest_django.fixtures import SettingsWrapper
 
-from ..context_processors import opal_global_settings
+from opal.core import context_processors
 
 
 def test_admin_url_in_context_processor(settings: SettingsWrapper) -> None:
@@ -10,6 +13,33 @@ def test_admin_url_in_context_processor(settings: SettingsWrapper) -> None:
     url = 'https://example.opal'
     settings.OPAL_ADMIN_URL = url
 
-    context = opal_global_settings(HttpRequest())
+    context = context_processors.opal_global_settings(HttpRequest())
     assert 'OPAL_ADMIN_URL' in context
     assert context.get('OPAL_ADMIN_URL') == url
+
+
+def test_current_app_no_resolver() -> None:
+    """Ensure that the `current_app` context processor can handle a `None` resolver_match."""
+    context = context_processors.current_app(HttpRequest())
+
+    assert 'app_verbose_name' not in context
+
+
+@pytest.mark.django_db()
+def test_current_app_verbose_name_no_app(client: Client) -> None:
+    """Ensure that the `current_app` context processor can return the current app's verbose name."""
+    response = client.get(reverse('start'))
+    # HttpResponse also
+    context = context_processors.current_app(response)  # type: ignore[arg-type]
+
+    assert 'app_verbose_name' not in context
+
+
+@pytest.mark.django_db()
+def test_current_app_verbose_name(client: Client) -> None:
+    """Ensure that the `current_app` context processor can return the current app's verbose name."""
+    response = client.get(reverse('admin:index'))
+    context = context_processors.current_app(response)  # type: ignore[arg-type]
+
+    assert 'app_verbose_name' in context
+    assert context['app_verbose_name'] == 'Administration'
