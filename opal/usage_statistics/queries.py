c9aa426200cb2788ca76b346fef11b21399bed8f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 02:09:45 +0000

    test: update unit tests
    (cherry picked from commit 4208f854921083d50e213ce7377ec1adfc3b6240)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/usage_statistics/queries.py
+++ opal/usage_statistics/queries.py
@@ -1,8 +1,9 @@
 """Statistics queries used by usage statistics app."""
+import datetime as dt
 from typing import Any
 
+from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 from opal.caregivers import models as caregivers_models
 from opal.patients import models as patients_models
@@ -10,12 +11,18 @@ from opal.patients import models as patients_models
 # GROUP REPORTING - POPULATION-LEVEL AGGREGATE STATISTICS
 
 
-# Population Summary
-def fetch_population_summary() -> dict[str, Any]:
-    """Fetch grouped user/caregiver population summary.
+def fetch_registration_summary(
+    start_datetime_period: dt.datetime,
+    end_datetime_period: dt.datetime,
+) -> dict[str, Any]:
+    """Fetch grouped registration summary from `RegistrationCode` model.
+
+    Args:
+        start_datetime_period: the beginning of the time period of the registration summary
+        end_datetime_period: the end of the time period of the registration summary
 
     Returns:
-        dictionary with aggregated summaries
+        registration summary for a given time period
     """
     incomplete_registration_statuses = [
         caregivers_models.RegistrationCodeStatus.NEW,
@@ -23,17 +30,11 @@ def fetch_population_summary() -> dict[str, Any]:
         caregivers_models.RegistrationCodeStatus.BLOCKED,
     ]
 
-    return caregivers_models.RegistrationCode.objects.select_related(
-        'relationship__type',
-        'relationship__patient',
-        'relationship__caregiver__user',
-    ).filter(
-        created_at__date=timezone.now().date(),
+    return caregivers_models.RegistrationCode.objects.filter(
+        created_at__gte=start_datetime_period,
+        created_at__lt=end_datetime_period,
     ).aggregate(
-        user_signed_up=models.Count(
-            'relationship__caregiver', distinct=True,
-        ),
-        incomplete_registration=models.Count(
+        uncompleted_registration=models.Count(
             'id',
             filter=models.Q(status__in=incomplete_registration_statuses),
         ),
@@ -41,40 +42,59 @@ def fetch_population_summary() -> dict[str, Any]:
             'id',
             filter=models.Q(status=caregivers_models.RegistrationCodeStatus.REGISTERED),
         ),
-        english=models.Count(
-            'id',
-            filter=models.Q(relationship__caregiver__user__language='en'),
-        ),
-        french=models.Count(
-            'id',
-            filter=models.Q(relationship__caregiver__user__language='fr'),
-        ),
-        deceased=models.Count(
-            'id',
-            filter=models.Q(relationship__patient__date_of_death__isnull=False),
-        ),
-        male=models.Count(
-            'id',
-            filter=models.Q(relationship__patient__sex=patients_models.SexType.MALE),
-        ),
-        female=models.Count(
-            'id',
-            filter=models.Q(relationship__patient__sex=patients_models.SexType.FEMALE),
-        ),
-        other_sex=models.Count(
-            'id',
-            filter=models.Q(relationship__patient__sex=patients_models.SexType.OTHER),
-        ),
-        unknown_sex=models.Count(
-            'id',
-            filter=models.Q(relationship__patient__sex=patients_models.SexType.UNKNOWN),
-        ),
-        full_access=models.Count(
-            'id',
-            filter=models.Q(relationship__patient__data_access=patients_models.DataAccessType.ALL),
-        ),
-        limit_access=models.Count(
-            'id',
-            filter=models.Q(relationship__patient__data_access=patients_models.DataAccessType.NEED_TO_KNOW),
-        ),
+    )
+
+
+def fetch_opal_users_summary(
+    start_datetime_period: dt.datetime,
+    end_datetime_period: dt.datetime,
+) -> dict[str, Any]:
+    """Fetch grouped Opal users summary from `CaregiverProfile` model.
+
+    Args:
+        start_datetime_period: the beginning of the time period of the users summary
+        end_datetime_period: the end of the time period of the users summary
+
+    Returns:
+        Opal users summary for a given time period
+    """
+    lang_codes = [lang[0] for lang in settings.LANGUAGES]
+    lang_dict = {
+        lang: models.Count('id', filter=models.Q(user__language=lang)) for lang in lang_codes
+    }
+    return caregivers_models.CaregiverProfile.objects.filter(
+        user__date_joined__gte=start_datetime_period,
+        user__date_joined__lt=end_datetime_period,
+    ).aggregate(
+        total=models.Count('id'),
+        **lang_dict,
+    )
+
+
+def fetch_patients_summary(
+    start_datetime_period: dt.datetime,
+    end_datetime_period: dt.datetime,
+) -> dict[str, Any]:
+    """Fetch grouped patients summary from `Patient` model.
+
+    Args:
+        start_datetime_period: the beginning of the time period of the patients summary
+        end_datetime_period: the end of the time period of the patients summary
+
+    Returns:
+        patients summary for a given time period
+    """
+    access_types = [access_type[0] for access_type in patients_models.DataAccessType.choices]
+    access_dict = {}
+    for access_type in access_types:
+        key_name = f'{access_type.lower()}_access'
+        access_dict[key_name] = models.Count('id', filter=models.Q(data_access=access_type))
+
+    return patients_models.Patient.objects.aggregate(
+        deceased=models.Count('id', filter=models.Q(date_of_death__isnull=False)),
+        male=models.Count('id', filter=models.Q(sex=patients_models.SexType.MALE)),
+        female=models.Count('id', filter=models.Q(sex=patients_models.SexType.FEMALE)),
+        other_sex=models.Count('id', filter=models.Q(sex=patients_models.SexType.OTHER)),
+        unknown_sex=models.Count('id', filter=models.Q(sex=patients_models.SexType.UNKNOWN)),
+        **access_dict,
     )
