1bc5524a76ec171e808fde4350f9a0c8443400a5
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:45:31 +0000

    feat deps add random ramq for patient factory
    (cherry picked from commit b6b2015bf295ae10fc32162860239337b77c5759)
    
    Co-authored-by: lymanliugd <Limin.Liu@muhc.mcgill.ca>

--- opal/patients/factories.py
+++ opal/patients/factories.py
@@ -9,6 +9,8 @@ reference from:
 
 import datetime
 
+from django.utils.crypto import get_random_string
+
 from factory import RelatedFactory, Sequence, SubFactory, lazy_attribute
 from factory.django import DjangoModelFactory
 
@@ -44,7 +46,10 @@ class Patient(DjangoModelFactory):
     last_name = 'Patient Last Name'
     date_of_birth = datetime.date(1999, 1, 1)
     sex = models.Patient.SexType.MALE
-    ramq = 'OTES12345678'
+    ramq = '{s}{n}'.format(
+        s=get_random_string(4, 'ABCDEFGHJKLMNPQRSTUVWXYZ'),
+        n=get_random_string(8, '1234567890'),
+    )
     legacy_id = Sequence(lambda number: number + 1)
 
 
