8b3757c51201ad6334c6f8bf211da1fe76f2ae6a
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:42:39 +0000

    Merge branch 'temp' into qsccd-428
    (cherry picked from commit e9b479fa236f521817f3ac8ec2e752322ab0be9a)
    
    Co-authored-by: lymanliugd <Limin.Liu@muhc.mcgill.ca>

---
+++ opal/caregivers/tests/test_managers.py
@@ -0,0 +1,19 @@
+import pytest
+
+from opal.patients import factories as patient_factory
+from opal.patients.models import Relationship
+from opal.users.models import Caregiver
+
+pytestmark = pytest.mark.django_db
+
+
+def test_caregiver_patient_list_query() -> None:
+    """Test the query to get the list of patients for a given caregiver."""
+    relationship_type = patient_factory.RelationshipType(name='Mother')
+    relationship = patient_factory.Relationship(type=relationship_type)
+    caregiver = Caregiver.objects.get()
+    query_result = Relationship.objects.get_patient_list_for_caregiver(caregiver.username)
+    assert query_result[0].patient_id == relationship.patient_id
+    assert query_result[0].caregiver_id == relationship.caregiver_id
+    assert query_result[0].type_id == relationship_type.id
+    assert relationship.type_id == relationship_type.id
