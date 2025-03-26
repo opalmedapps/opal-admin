6ba569dbf2f0d525789d616555171856c703b036
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:14:22 +0000

    Merge branch 'main' into QSCCD-927.LL.fix-appointment-api
    (cherry picked from commit b85173f6d3a3d9474a55cf00590c339d296d2c49)
    
    Co-authored-by: LiminLiu <Limin.Liu@muhc.mcgill.ca>

--- opal/core/context_processors.py
+++ opal/core/context_processors.py
@@ -1,11 +1,12 @@
 """This module provides custom context processors for this project."""
-from typing import Dict
+from typing import Any
 
+from django.apps import apps
 from django.conf import settings
 from django.http import HttpRequest
 
 
-def opal_global_settings(request: HttpRequest) -> Dict:
+def opal_global_settings(request: HttpRequest) -> dict[str, Any]:
     """
     Provide custom context processor that returns a dictionary with the `OpalAdmin` global values.
 
@@ -13,8 +14,31 @@ def opal_global_settings(request: HttpRequest) -> Dict:
         request: `HttpRequest` object
 
     Returns:
-        dictionary that contains `OpalAdmin` global values (e.g., OpalAdmin URL).
+        dictionary that contains `OpalAdmin` global values (e.g., OpalAdmin URL)
     """
     return {
         'OPAL_ADMIN_URL': settings.OPAL_ADMIN_URL,
     }
+
+
+def current_app(request: HttpRequest) -> dict[str, Any]:
+    """
+    Provide custom context processor that returns information about the current app.
+
+    The app name can already be retrieved via `request.resolver_match.app_name`.
+
+    Args:
+        request: `HttpRequest` object
+
+    Returns:
+        dictionary that current app information
+    """
+    context: dict[str, Any] = {}
+
+    if request.resolver_match:
+        app_name = request.resolver_match.app_name.replace('-', '_')
+
+        if app_name:
+            context['app_verbose_name'] = apps.get_app_config(app_name).verbose_name
+
+    return context
