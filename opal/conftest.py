bb395103e2ad929e70341f7916f9a31da2b5b330
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:03:47 +0000

    test: add unit tests for front end and back end permissions to relationshiptypes
    (cherry picked from commit 690c3f7d3ac555ca27cd2d289315052f4edd8cfe)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/conftest.py
+++ opal/conftest.py
@@ -42,8 +42,10 @@ def user_client(client: Client, django_user_model: User) -> Client:
     user = django_user_model.objects.create(username='testuser')
     manage_institution_permission = Permission.objects.get(codename='can_manage_institutions')
     manage_relationship_permission = Permission.objects.get(codename='can_manage_relationships')
+    manage_relationshiptype_permission = Permission.objects.get(codename='can_manage_relationshiptypes')
     user.user_permissions.add(manage_institution_permission)
     user.user_permissions.add(manage_relationship_permission)
+    user.user_permissions.add(manage_relationshiptype_permission)
     client.force_login(user)
 
     return client
