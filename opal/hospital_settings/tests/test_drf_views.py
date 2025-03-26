484fbfe54e11abf34a4e730996a853bd68356497
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:51:51 +0000

    improve hospital settings API views
    (cherry picked from commit 397e05dabfec3f2c674f2d1dae9db03644ba38cd)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- opal/hospital_settings/tests/test_drf_views.py
+++ opal/hospital_settings/tests/test_drf_views.py
@@ -1,9 +1,12 @@
 from http import HTTPStatus
+from typing import Callable
 
 from django.urls.base import reverse
 
 import pytest
 from rest_framework.test import APIClient
+
+from opal.users.models import User
 
 from .. import factories
 
@@ -24,18 +27,26 @@ def test_api_institution_no_permission(user_api_client: APIClient) -> None:
     assert response.status_code == HTTPStatus.FORBIDDEN
 
 
-def test_api_institution_not_found(admin_api_client: APIClient) -> None:
+def test_api_institution_not_found(
+    api_client: APIClient,
+    user_with_permission: Callable[[str | list[str]], User],
+) -> None:
     """Ensure that a 404 is returned if there is no institution."""
-    response = admin_api_client.get(reverse('api:institution-detail'))
+    api_client.force_login(user_with_permission('hospital_settings.view_institution'))
+    response = api_client.get(reverse('api:institution-detail'))
 
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_api_institution(admin_api_client: APIClient) -> None:
+def test_api_institution(
+    api_client: APIClient,
+    user_with_permission: Callable[[str | list[str]], User],
+) -> None:
     """Ensure that the singleton institution is returned."""
     institution = factories.Institution(name='Test', code='TST')
+    api_client.force_login(user_with_permission('hospital_settings.view_institution'))
 
-    response = admin_api_client.get(reverse('api:institution-detail'))
+    response = api_client.get(reverse('api:institution-detail'))
 
     assert response.status_code == HTTPStatus.OK
 
@@ -45,11 +56,15 @@ def test_api_institution(admin_api_client: APIClient) -> None:
     assert data['code'] == 'TST'
 
 
-def test_api_institution_multiple_institutions(admin_api_client: APIClient) -> None:
+def test_api_institution_multiple_institutions(
+    api_client: APIClient,
+    user_with_permission: Callable[[str | list[str]], User],
+) -> None:
     """Ensure that the singleton institution is returned."""
     factories.Institution()
     factories.Institution(name='Test', code='TST')
+    api_client.force_login(user_with_permission('hospital_settings.view_institution'))
 
-    response = admin_api_client.get(reverse('api:institution-detail'))
+    response = api_client.get(reverse('api:institution-detail'))
 
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
