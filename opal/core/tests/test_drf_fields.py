b4f8bfab693d5977733534dfeea60ac092699092
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:14:45 +0000

    add comment on the class
    (cherry picked from commit 41544be716e5f7c2bbda1a4decd98cda70950f4a)
    
    Co-authored-by: Zeyu Dou <dou3180@hotmail.com>

--- opal/core/tests/test_drf_fields.py
+++ opal/core/tests/test_drf_fields.py
@@ -8,17 +8,33 @@ from ..drf_fields import Base64FileField
 
 # copied from: https://github.com/Hipo/drf-extra-fields/blob/master/tests/test_fields.py
 class _FieldFile:
+    """A fake FieldFile class in purpose of unit testing."""
+
     def __init__(self, path: str):
+        """
+        Set the fake FieldFile class.
+
+        Args:
+            path: tmp path for testing in string format
+        """
         self.path = path
 
 
 class _DownloadableBase64File:
+    """The class to instantiated the fake FieldFile class."""
+
     def __init__(self, file_path: Path):
+        """
+        Set the DownloadableBase64File class.
+
+        Args:
+            file_path: the path Oject for FieldFile class.
+        """
         self.file = _FieldFile(path=str(file_path))
 
 
 def test_base64filefield_to_representation(tmp_path: Path) -> None:
-    """The `Base64FileField` returns the base64 encoded content of the file."""
+    """Ensure the `Base64FileField` returns the base64 encoded content of the file."""
     test_file = tmp_path.joinpath('test.txt')
     with test_file.open('wb') as fd:
         fd.write(b'test')
