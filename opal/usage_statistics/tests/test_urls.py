094e1da82132066aa0cf234fbbd784d5efc10b9a
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 02:20:50 +0000

    test: add unit tests
    (cherry picked from commit 96cc3f3bb33f7022238922ade73d8cb0215b9e02)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/usage_statistics/tests/test_urls.py
+++ opal/usage_statistics/tests/test_urls.py
@@ -1,11 +1,25 @@
 from django.urls import resolve, reverse
+from django.views.generic.base import View
+
+import pytest
+from pytest_django.asserts import assertURLEqual
+
+from .. import views
+
+TestData = tuple[type, str, str]
+
+# tuple with expected data (view class, url name, url path)
+testdata: list[TestData] = [
+    (views.UsageStatisticsExportTemplateView, 'usage-statistics:data-export', '/usage-statistics/export/'),
+]
 
 
-def test_usage_statistics_export_url() -> None:
+@pytest.mark.parametrize(('view_class', 'url_name', 'url_path'), testdata)
+def test_usage_statistics_urls(view_class: type[View], url_name: str, url_path: str) -> None:
     """
     Ensure that usage statistics export URL name resolves to the appropriate URL address.
 
     It also checks that the URL is served with the correct view.
     """
-    assert reverse('usage-statistics:data-export') == '/usage-statistics/export/'
-    assert resolve('/usage-statistics/export/').view_name == 'usage-statistics:data-export'
+    assertURLEqual(reverse(url_name), url_path)
+    assert resolve(url_path).func.__name__ == view_class.as_view().__name__
