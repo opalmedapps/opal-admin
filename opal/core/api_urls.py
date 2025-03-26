c554a32f9d8e6d123281b0eff0d04ab148bf9259
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:48:25 +0000

    feat: create api to create and edit clinical staff users
    (cherry picked from commit 4d573266836f04b4400d98d5ce45cc94965ee5b1)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/core/api_urls.py
+++ opal/core/api_urls.py
@@ -27,10 +27,12 @@ from opal.legacy.api.views.questionnaires_report import QuestionnairesReportView
 from opal.patients.api import views as patient_views
 from opal.test_results.api.views import CreatePathologyView
 from opal.users.api import views as user_views
+from opal.users.api import viewsets as user_viewsets
 
 # show APIRootView only in debug mode
 # add trailing_slash=False if the trailing slash should not be enforced
 # see: https://www.django-rest-framework.org/api-guide/routers/#defaultrouter
+
 if settings.DEBUG:
     router: SimpleRouter = DefaultRouter()
 else:
@@ -40,7 +42,7 @@ else:
 router.register('institutions', settings_viewsets.InstitutionViewSet, basename='institutions')
 router.register('sites', settings_viewsets.SiteViewSet, basename='sites')
 router.register('security-questions', SecurityQuestionViewSet, basename='security-questions')
-
+router.register('users', user_viewsets.UserViewSet, basename='users')
 
 app_name = 'core'
 
