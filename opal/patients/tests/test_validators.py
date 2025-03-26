94dae9dd361c2ec29fc535dcb6f00b8174850c2c
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:29:32 +0000

    fix: add is_deceased  tests
    (cherry picked from commit 60db6818d3771163898e86819a5495f0e6c18db4)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/patients/tests/test_validators.py
+++ opal/patients/tests/test_validators.py
@@ -1,8 +1,10 @@
 from datetime import date, datetime
 
+from django.utils import timezone
+
 from opal.services.hospital.hospital_data import OIEMRNData, OIEPatientData
 
-from .. import validators
+from .. import factories, validators
 
 CUSTOMIZED_OIE_PATIENT_DATA = OIEPatientData(
     date_of_birth=date.fromisoformat('1984-05-09'),
@@ -104,3 +106,31 @@ def test_no_mrns_have_same_site_code() -> None:
         ],
     )
     assert validators.has_multiple_mrns_with_same_site_code(patient_data) is False
+
+
+def test_patient_validator_not_deceased_oie_patient() -> None:
+    """Ensure `is_deceased` returns False when patients are not deceased for OIE patients."""
+    oie_patient = CUSTOMIZED_OIE_PATIENT_DATA
+
+    assert validators.is_deceased(oie_patient) is False
+
+
+def test_patient_validator_is_deceased_oie_patient() -> None:
+    """Ensure deceased patients are caught in the validator for oie patients."""
+    oie_patient = CUSTOMIZED_OIE_PATIENT_DATA._replace(deceased=True)
+
+    assert validators.is_deceased(oie_patient) is True
+
+
+def test_patient_validator_is_deceased_patient_model() -> None:
+    """Ensure deceased patients are caught in the validator for patients of `Patient` model."""
+    patient = factories.Patient(date_of_death=timezone.now())
+
+    assert validators.is_deceased(patient) is True
+
+
+def test_patient_validator_not_deceased_patient_model() -> None:
+    """Ensure `is_deceased` returns False when patients are not deceased in `Patient` model."""
+    patient = factories.Patient()
+
+    assert validators.is_deceased(patient) is False
