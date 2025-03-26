d33d604f9bcee5d1b57a7883c99e84e120463584
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:44:13 +0000

    fix: Remove `translation.override`; add condition that checks `Accept-Language` header
    (cherry picked from commit c898fbd521aaf45364cc5c7120d1c3a9326c3f97)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/legacy/api/views/questionnaires_report.py
+++ opal/legacy/api/views/questionnaires_report.py
@@ -4,7 +4,7 @@ import logging
 from pathlib import Path
 from typing import Any
 
-from django.utils import timezone, translation
+from django.utils import timezone
 
 from rest_framework import permissions
 from rest_framework import request as rest_request
@@ -47,51 +47,53 @@ class QuestionnairesReportCreateAPIView(APIView):
         Returns:
             HTTP `Response` with results of report generation
         """
-        with translation.override(request.headers['Accept-Language']):  # TODO: override the language in a middleware
-            serializer = QuestionnaireReportRequestSerializer(data=request.data)
-            # Validate received data. Return a 400 response if the data was invalid.
-            serializer.is_valid(raise_exception=True)
+        if 'Accept-Language' not in request.headers:
+            return self._create_error_response('The request does not contain the "Accept-Language" header.')
 
-            # Generate questionnaire report
-            encoded_report = self.report_service.generate_questionnaire_report(
-                reports.QuestionnaireReportRequestData(
-                    patient_id=serializer.validated_data.get('patient_id'),
-                    logo_path=Path(Institution.objects.get(pk=1).logo.path),
-                    language=request.headers['Accept-Language'],
-                ),
+        serializer = QuestionnaireReportRequestSerializer(data=request.data)
+        # Validate received data. Return a 400 response if the data was invalid.
+        serializer.is_valid(raise_exception=True)
+
+        # Generate questionnaire report
+        encoded_report = self.report_service.generate_questionnaire_report(
+            reports.QuestionnaireReportRequestData(
+                patient_id=serializer.validated_data.get('patient_id'),
+                logo_path=Path(Institution.objects.get(pk=1).logo.path),
+                language=request.headers['Accept-Language'],
+            ),
+        )
+
+        # The `ReportService` does not return error messages.
+        # If an error occurs during report generation, the `ReportService` returns `None`
+        if not encoded_report:
+            return self._create_error_response('An error occurred during report generation.')
+
+        hospital_patient = HospitalPatient.objects.select_related(
+            'site',
+        ).filter(
+            patient__legacy_id=serializer.validated_data.get('patient_id'),
+        ).first()
+
+        if not hospital_patient:
+            return self._create_error_response(
+                'Could not find `HospitalPatient` object for the given `patient_id`.',
             )
 
-            # The `ReportService` does not return error messages.
-            # If an error occurs during report generation, the `ReportService` returns `None`
-            if not encoded_report:
-                return self._create_error_response('An error occurred during report generation.')
+        # Submit report to the OIE
+        export_result = self.oie.export_pdf_report(
+            hospital.OIEReportExportData(
+                mrn=hospital_patient.mrn,
+                site=hospital_patient.site.name,
+                base64_content=encoded_report,
+                document_number='FMU',  # TODO: clarify where to get the value
+                document_date=timezone.localtime(timezone.now()),  # TODO: get the exact time of the report creation
+            ),
+        )
 
-            hospital_patient = HospitalPatient.objects.select_related(
-                'site',
-            ).filter(
-                patient__legacy_id=serializer.validated_data.get('patient_id'),
-            ).first()
+        if 'status' not in export_result or export_result['status'] == 'error':
+            self.logger.error('An error occurred while exporting a PDF report to the OIE.')
 
-            if not hospital_patient:
-                return self._create_error_response(
-                    'Could not find `HospitalPatient` object for the given `patient_id`.',
-                )
-
-            # Submit report to the OIE
-            export_result = self.oie.export_pdf_report(
-                hospital.OIEReportExportData(
-                    mrn=hospital_patient.mrn,
-                    site=hospital_patient.site.name,
-                    base64_content=encoded_report,
-                    document_number='FMU',  # TODO: clarify where to get the value
-                    document_date=timezone.localtime(timezone.now()),  # TODO: get the exact time of the report creation
-                ),
-            )
-
-            if 'status' not in export_result or export_result['status'] == 'error':
-                self.logger.error('An error occurred while exporting a PDF report to the OIE.')
-
-            return response.Response(export_result)
+        return response.Response(export_result)
 
     def _create_error_response(self, message: str) -> response.Response:
         # Log an error message
