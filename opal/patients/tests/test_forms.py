1b244c5e44730c4dc9f500468d3a274360c527e9
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:01:31 +0000

    fix: remove conftest and moved all data initializations into tests themselves as suggested
    (cherry picked from commit cfa20c6e312caaad25f2324d8b66eed87f4ff146)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/patients/tests/test_forms.py
+++ opal/patients/tests/test_forms.py
@@ -1,54 +1,97 @@
+import datetime
+
+from django.forms import model_to_dict
+
 import pytest
 
+from .. import factories
 from ..forms import RelationshipPendingAccessForm
-from ..models import Relationship
+from ..models import Relationship, RelationshipStatus
 
 pytestmark = pytest.mark.django_db
 
 
-def test_relationshippending_form_is_valid(relationshippending_form: RelationshipPendingAccessForm) -> None:
+def test_relationshippending_form_is_valid() -> None:
     """Ensure that the `RelationshipPendingAccess` form is valid."""
+    relationship_info = factories.Relationship.create()
+    form_data = model_to_dict(relationship_info)
+
+    relationshippending_form = RelationshipPendingAccessForm(data=form_data, instance=relationship_info)
+
     assert relationshippending_form.is_valid()
 
 
-def test_relationshippending_missing_startdate(
-    incomplete_relationshippending_form: RelationshipPendingAccessForm,
-) -> None:
+def test_relationshippending_missing_startdate() -> None:
     """Ensure that the `RelationshipPendingAccess` form checks for a missing start date field."""
-    assert not incomplete_relationshippending_form.is_valid()
+    relationship_info = factories.Relationship.build()
+    form_data = model_to_dict(relationship_info, exclude=[
+        'start_date',
+        'end_date',
+    ])
+
+    relationshippending_form = RelationshipPendingAccessForm(data=form_data, instance=relationship_info)
+    assert not relationshippending_form.is_valid()
 
 
-def test_relationshippending_update(relationshippending_form: RelationshipPendingAccessForm) -> None:
+def test_relationshippending_update() -> None:
     """Ensure that a valid `RelationshipPendingAccess` form can be saved."""
+    relationship_info = factories.Relationship.create()
+    form_data = model_to_dict(relationship_info)
+
+    relationshippending_form = RelationshipPendingAccessForm(data=form_data, instance=relationship_info)
     relationshippending_form.save()
+
     assert Relationship.objects.all()[0].start_date == relationshippending_form.data['start_date']
 
 
-def test_relationshippending_update_fail(
-    incomplete_relationshippending_form: RelationshipPendingAccessForm,
-) -> None:
+def test_relationshippending_update_fail() -> None:
     """Ensure that the `RelationshipPendingAccess` form checks for a missing start date field."""
+    relationship_info = factories.Relationship.build()
+    form_data = model_to_dict(relationship_info, exclude=[
+        'start_date',
+        'end_date',
+    ])
+
     message = 'This field is required.'
+    relationshippending_form = RelationshipPendingAccessForm(data=form_data, instance=relationship_info)
 
-    assert not incomplete_relationshippending_form.is_valid()
-    assert incomplete_relationshippending_form.errors['start_date'][0] == message
+    assert not relationshippending_form.is_valid()
+    assert relationshippending_form.errors['start_date'][0] == message
 
 
-def test_relationshippending_form_date_validated(
-    wrong_date_relationshippending_form: RelationshipPendingAccessForm,
-) -> None:
+def test_relationshippending_form_date_validated() -> None:
     """Ensure that the `RelationshipPendingAccess` form is validated for startdate>enddate."""
+    relationship_info = factories.Relationship.build(
+        patient=factories.Patient(),
+        caregiver=factories.CaregiverProfile(),
+        type=factories.RelationshipType(),
+        start_date=datetime.date(2022, 6, 1),  # noqa: WPS432
+        end_date=datetime.date(2022, 5, 1),  # noqa: WPS432
+    )
+    form_data = model_to_dict(relationship_info)
+
     message = 'Start date should be earlier than end date.'
+    relationshippending_form = RelationshipPendingAccessForm(data=form_data, instance=relationship_info)
 
-    assert not wrong_date_relationshippending_form.is_valid()
-    assert wrong_date_relationshippending_form.errors['start_date'][0] == message
+    assert not relationshippending_form.is_valid()
+    assert relationshippending_form.errors['start_date'][0] == message
 
 
-def test_relationshippending_status_reason(
-    empty_reason_denied_relationshippending_form: RelationshipPendingAccessForm,
-) -> None:
+def test_relationshippending_status_reason() -> None:
     """Ensure that the `RelationshipPendingAccess` form is validated for reason is not empty when status is denied."""
-    message = 'Reason is mandatory when status is denied or revoked.'
+    relationship_info = factories.Relationship.build(
+        patient=factories.Patient(),
+        caregiver=factories.CaregiverProfile(),
+        type=factories.RelationshipType(),
+        status=RelationshipStatus.DENIED,
+        start_date=datetime.date(2022, 5, 1),  # noqa: WPS432
+        end_date=datetime.date(2022, 6, 1),  # noqa: WPS432
+        reason='',
+    )
+    form_data = model_to_dict(relationship_info)
 
-    assert not empty_reason_denied_relationshippending_form.is_valid()
-    assert empty_reason_denied_relationshippending_form.errors['reason'][0] == message
+    message = 'Reason is mandatory when status is denied or revoked.'
+    relationshippending_form = RelationshipPendingAccessForm(data=form_data, instance=relationship_info)
+
+    assert not relationshippending_form.is_valid()
+    assert relationshippending_form.errors['reason'][0] == message
