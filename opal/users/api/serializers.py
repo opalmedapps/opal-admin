f2be51b55ecfb32fa8811e4205f748fdf6c4ee8e
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 02:22:49 +0000

    Merge branch 'main' into QSCCD-2234.AG.add-individual-reports-form
    (cherry picked from commit b40cffe02da9190d830f7ab76957cdcd1caa8eba)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/users/api/serializers.py
+++ opal/users/api/serializers.py
@@ -1,5 +1,10 @@
 """This module provides Django REST framework serializers for User-specific models."""
+from typing import Any
+
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group
+from django.contrib.auth.password_validation import validate_password as auth_validate_password
+from django.utils.translation import gettext
 
 from rest_framework import serializers
 
@@ -56,15 +61,75 @@ class UserClinicalStaffSerializer(serializers.ModelSerializer[ClinicalStaff]):
     """
     User ClinicalStaff serializer.
 
-    The serializer is used to create new clinical staff user and assigned them to group(s).
+    The serializer is used to create a new clinical staff user and assign them to group(s).
     """
+
+    password = serializers.CharField(write_only=True, required=False)
+    password2 = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = ClinicalStaff
-        fields = ('username', 'groups')
+        fields = ('username', 'password', 'password2', 'groups')
+
+    def validate_password(self, password: str) -> str:
+        """
+        Validate that the password follows the password requirements.
+
+        Raises a `ValidationError` for any violation.
+
+        Args:
+            password: the raw password
+
+        Returns:
+            the validated password
+        """
+        auth_validate_password(password)
+
+        return password
+
+    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
+        """
+        Validate all data.
+
+        Verifies that the passwords are the same.
+
+        Args:
+            data: the request data
+
+        Returns:
+            the validated data
+
+        Raises:
+            ValidationError: if the passwords are not the same
+        """
+        if (
+            'password' in data
+            and 'password2' in data
+            and data['password'] != data['password2']
+        ):
+            raise serializers.ValidationError(gettext("The two password fields don't match."))
+
+        return data
+
+    def save(self, **kwargs: Any) -> ClinicalStaff:
+        """
+        Save the validated data.
+
+        Args:
+            kwargs: additional keyword arguments
+
+        Returns:
+            the created or updated clinical staff instance
+        """
+        # handle passwords if provided
+        if 'password' in self.validated_data:
+            self.validated_data.pop('password2')
+            self.validated_data['password'] = make_password(self.validated_data['password'])  # noqa: WPS529
+
+        return super().save(**kwargs)
 
 
-class UpdateClinicalStaffUserSerializer(serializers.ModelSerializer[ClinicalStaff]):
+class UpdateClinicalStaffGroupSerializer(serializers.ModelSerializer[ClinicalStaff]):
     """Serializer to retrieve and update the clinical staff users' groups."""
 
     class Meta:
