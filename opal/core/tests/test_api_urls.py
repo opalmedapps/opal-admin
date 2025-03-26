cd59126296b48909cf87b8b78b7f123c82201a6c
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:33:51 +0000

    test: CI test for pytest
    (cherry picked from commit bb83b24a1c07db0d1228b1392b5d0615689e625a)
    
    Co-authored-by: Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>

--- opal/core/tests/test_api_urls.py
+++ opal/core/tests/test_api_urls.py
@@ -1,3 +1,4 @@
+from django.test import Client
 from django.urls import resolve, reverse
 from django.urls.exceptions import NoReverseMatch, Resolver404
 
@@ -7,11 +8,16 @@ from pytest_django.fixtures import SettingsWrapper
 
 # note: cannot test the opposite (APIRootView defined in debug mode)
 # since the first test loads the URLs and the router is defined at that time
-def test_api_root_debug_only(settings: SettingsWrapper) -> None:
+@pytest.mark.django_db()
+def test_api_root_debug_only(client: Client, settings: SettingsWrapper) -> None:
     """Ensure that the API root is not available when not in debug mode."""
     assert settings.DEBUG is False
 
     path = '/{api_root}/'.format(api_root=settings.API_ROOT)
+
+    response = client.get(reverse('api:api-root'))
+    print(response)
+    assert False
     with pytest.raises(NoReverseMatch):
         reverse('api:api-root')
     with pytest.raises(Resolver404):
