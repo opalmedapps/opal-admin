733e234687ae2b24df66b3c5bb914ba9fcc8f8cf
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:36:18 +0000

    fix test
    (cherry picked from commit d46370dd2f0206afc940e8e8a5c6e61df4ddd41b)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- opal/tests/sql/test_QuestionnaireDB.sql
+++ opal/tests/sql/test_QuestionnaireDB.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd409bd83013eeb285772b1fbee393afad088f4e6ab6187a9f9a23b4acecf3dc
+oid sha256:c5046ca8e462e1332d662289b95dd129d1b8a7c6dcfa9bf98e00b1295d1254f9
 size 3904953
