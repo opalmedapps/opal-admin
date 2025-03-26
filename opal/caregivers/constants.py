c61518dc579becdc97df56f8f41fa564581de63e
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:59:46 +0000

    Apply 1 suggestion(s) to 1 file(s)
    (cherry picked from commit 6dd644b1f615017a51b04eece7ff53fe824eb37d)
    
    Co-authored-by: Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>

--- opal/caregivers/constants.py
+++ opal/caregivers/constants.py
@@ -3,7 +3,7 @@ from typing import Final
 
 #: Maximum length verification code for an email_verification.
 VERIFICATION_CODE_LENGTH: Final[int] = 6
-#: Time delay in seconds for sending email (in seconds).
+#: Time delay before re-sending a verification code by email (in seconds).
 CODE_RESEND_TIME_DELAY: Final[int] = 10
 #: The email verification code timeout in minutes.
 EMAIL_VERIFICATION_TIMEOUT: Final[int] = 30
