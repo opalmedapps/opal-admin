a7aa3f7d357cfdfaaa6d369e540a1ddd52989fcc
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:35:28 +0000

    fix: catch connection issues when sending an SMS
    (cherry picked from commit ca9979ddc8c761427cd465a01074ba36c9eb5e60)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- opal/patients/forms.py
+++ opal/patients/forms.py
@@ -18,6 +18,7 @@ from crispy_forms.layout import HTML, Column, Div
 from crispy_forms.layout import Field as CrispyField
 from crispy_forms.layout import Hidden, Layout, Row, Submit
 from dynamic_forms import DynamicField, DynamicFormMixin
+from requests.exceptions import RequestException
 
 from opal.caregivers.models import CaregiverProfile
 from opal.core import validators
@@ -726,7 +727,7 @@ class AccessRequestSendSMSForm(forms.Form):
 
             try:
                 twilio.send_sms(phone_number, message)
-            except TwilioServiceError:
+            except (TwilioServiceError, RequestException):
                 self.add_error(NON_FIELD_ERRORS, gettext('An error occurred while sending the SMS'))
                 LOGGER.exception(f'Sending SMS failed to {phone_number}')
 
