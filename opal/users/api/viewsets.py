3ab550c632839d4ff77a5a0cf5595bd182af204b
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:49:11 +0000

    feat: add action to set and unset users groups
    (cherry picked from commit 7d46e67ad730e4a9c9e8ddb09dba1fc42ba44953)
    
    Co-authored-by: Ammar Alsaig <ammar.alsaig@muhc.mcgill.ca>

--- opal/users/api/viewsets.py
+++ opal/users/api/viewsets.py
@@ -1,10 +1,19 @@
 """This module provides `ViewSets` for the users app."""
-
+from http import HTTPStatus
 from typing import Type
 
+from django.contrib.auth.models import Group
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.serializers import BaseSerializer
+
+from config.settings.base import USER_MANAGER_GROUP_NAME
 
 from ...core.drf_permissions import CustomDjangoModelPermissions
 from ..models import ClinicalStaff
@@ -30,10 +39,67 @@ class UserViewSet(
         'create': UserClinicalStaffSerializer,
         'retrieve': UpdateClinicalStaffUserSerializer,
         'update': UpdateClinicalStaffUserSerializer,
+        'set_manager_user': UpdateClinicalStaffUserSerializer,
     }
     permission_classes = [CustomDjangoModelPermissions]
 
     default_serializer_class = UserClinicalStaffSerializer
+
+    @action(detail=True, methods=['post'])
+    def set_manager_user(self, request: Request, username: str = '') -> Response:
+        """
+        Handle requests for setting a user group.
+
+        Add the passed user to a predefined managers group.
+
+        Args:
+            request: Http request.
+            username: user username.
+
+        Returns:
+            Http response with response details.
+        """
+        try:
+            clinicalstaff_user = ClinicalStaff.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return Response({'detail': _('user not found.')}, status=HTTPStatus.NOT_FOUND)
+
+        try:
+            group = Group.objects.get(name=USER_MANAGER_GROUP_NAME)
+        except ObjectDoesNotExist:
+            return Response({'detail': _('manager group not found.')}, status=HTTPStatus.NOT_FOUND)
+
+        clinicalstaff_user.groups.add(group.pk)
+        clinicalstaff_user.save()
+        return Response({'detail': _('user was added to the managers group successfully.')}, status=HTTPStatus.OK)
+
+    @action(detail=True, methods=['post'])
+    def unset_manager_user(self, request: Request, username: str = '') -> Response:
+        """
+        Handle requests for unsetting a user group.
+
+        Remove the passed user from a predefined managers group.
+
+        Args:
+            request: Http request.
+            username: user username.
+
+        Returns:
+            Http response with response details.
+        """
+        try:
+            clinicalstaff_user = ClinicalStaff.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return Response({'detail': _('user not found.')}, status=HTTPStatus.NOT_FOUND)
+
+        try:
+            group = Group.objects.get(name=USER_MANAGER_GROUP_NAME)
+        except ObjectDoesNotExist:
+            return Response({'detail': _('manager group not found.')}, status=HTTPStatus.NOT_FOUND)
+
+        clinicalstaff_user.groups.remove(group.pk)
+        clinicalstaff_user.save()
+        return Response({'detail': _('user was removed from the managers group successfully.')}, status=HTTPStatus.OK)
 
     def get_serializer_class(self) -> Type[BaseSerializer]:
         """
