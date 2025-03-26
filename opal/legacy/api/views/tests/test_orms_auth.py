d26b1e64399f6d3321d0f88222dc945a4eb13c60
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:06:43 +0000

    test: add unit tests
    (cherry picked from commit be1e020bbc7e21825880304500294b139298ad3d)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/legacy/api/views/tests/test_orms_auth.py
+++ opal/legacy/api/views/tests/test_orms_auth.py
@@ -37,7 +37,48 @@ class TestORMSLoginView:
 
         assertContains(response, 'key')
 
-    def test_orms_unable_to_login(
+    def test_orms_login_with_missing_credentials(
+        self,
+        api_client: APIClient,
+        settings: SettingsWrapper,
+    ) -> None:
+        """Ensure the login endpoint returns an error if user does not provide username and password."""
+        settings.AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
+        response = api_client.post(
+            reverse('api:orms-login'),
+            data={},
+            format='json',
+        )
+
+        assertContains(
+            response=response,
+            text='This field is required.',
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def test_orms_login_with_wrong_credentials(
+        self,
+        api_client: APIClient,
+        settings: SettingsWrapper,
+    ) -> None:
+        """Ensure the login endpoint returns an error if user provides wrong username and password."""
+        settings.AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
+        response = api_client.post(
+            reverse('api:orms-login'),
+            data={
+                'username': 'wronguser',
+                'password': 'wrongpass',
+            },
+            format='json',
+        )
+
+        assertContains(
+            response=response,
+            text='Unable to log in with provided credentials.',
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def test_orms_forbidden_to_login(
         self,
         api_client: APIClient,
         django_user_model: AbstractUser,
@@ -58,6 +99,49 @@ class TestORMSLoginView:
 
         assertContains(
             response=response,
+            text='You do not have permission to perform this action.',
+            status_code=status.HTTP_403_FORBIDDEN,
+        )
+
+    def test_orms_multiple_requests(
+        self,
+        api_client: APIClient,
+        django_user_model: AbstractUser,
+        settings: SettingsWrapper,
+    ) -> None:
+        """Ensure the endpoint handles different requests properly."""
+        settings.AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
+        orms_group = Group.objects.create(name=settings.ORMS_USER_GROUP)
+        orms_user = django_user_model.objects.create(username='ormsuser')
+        orms_user.set_password('ormspass')
+        orms_user.groups.add(orms_group)
+        orms_user.save()
+
+        user = django_user_model.objects.create(username='testuser')
+        user.set_password('testpass')
+        user.save()
+
+        orms_user_response = api_client.post(
+            reverse('api:orms-login'),
+            data={
+                'username': 'ormsuser',
+                'password': 'ormspass',
+            },
+            format='json',
+        )
+
+        user_response = api_client.post(
+            reverse('api:orms-login'),
+            data={
+                'username': 'testuser',
+                'password': 'testpass',
+            },
+            format='json',
+        )
+
+        assertContains(orms_user_response, 'key')
+        assertContains(
+            user_response,
             text='You do not have permission to perform this action.',
             status_code=status.HTTP_403_FORBIDDEN,
         )
