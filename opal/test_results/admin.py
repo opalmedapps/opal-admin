b166e737c9fa4f00507e28ac66a218fa0fbb0977
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:43:31 +0000

    adjust search fields
    (cherry picked from commit 904480a2c08bb454c83e6ecf0a5a20b112174e44)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- opal/test_results/admin.py
+++ opal/test_results/admin.py
@@ -13,9 +13,7 @@ class GeneralTestAdmin(admin.ModelAdmin):
     search_fields = (
         'patient__first_name',
         'patient__last_name',
-        'patient__ramq',
-        'sending_facility',
-        'receiving_facility',
+        'collected_at',
     )
     ordering = ('patient', '-collected_at')
 
@@ -35,23 +33,22 @@ class ObservationAdmin(admin.ModelAdmin):
     search_fields = (
         'general_test__patient__first_name',
         'general_test__patient__last_name',
-        'general_test__patient__ramq',
         'identifier_code',
         'identifier_text',
+        'observed_at',
     )
-    ordering = ('general_test', '-updated_at')
+    ordering = ('general_test', '-observed_at')
 
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     """The admin class for `Note` models."""
 
-    list_display = ('note_text', 'general_test', 'note_source', 'updated_at')
+    list_display = ('general_test', 'note_text', 'note_source', 'updated_at')
     list_filter = ('general_test',)
     search_fields = (
         'general_test__patient__first_name',
         'general_test__patient__last_name',
-        'general_test__patient__ramq',
         'note_source',
         'note_text',
     )
