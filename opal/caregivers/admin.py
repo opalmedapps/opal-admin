c2f081c7b89cdef074f4a8347640d58bc671a896
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:17:12 +0000

    add RegistrationCode modeladmin
    (cherry picked from commit 66eaa5656ad57715fb70fe9c1199162a30330ba5)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- opal/caregivers/admin.py
+++ opal/caregivers/admin.py
@@ -12,6 +12,12 @@ class CaregiverProfileAdmin(admin.ModelAdmin):
     readonly_fields = ['uuid']
 
 
+class RegistrationCodeAdmin(admin.ModelAdmin):
+    """Admin options for the `RegistrationCode` model."""
+
+    readonly_fields = ['created_at']
+
+
 class SecurityQuestionAdmin(TranslationAdmin):
     """This class provides admin options for `SecurityQuestion`."""
 
@@ -19,7 +25,7 @@ class SecurityQuestionAdmin(TranslationAdmin):
 
 
 admin.site.register(models.CaregiverProfile, CaregiverProfileAdmin)
-admin.site.register(models.RegistrationCode, admin.ModelAdmin)
+admin.site.register(models.RegistrationCode, RegistrationCodeAdmin)
 admin.site.register(models.SecurityQuestion, SecurityQuestionAdmin)
 admin.site.register(models.SecurityAnswer, admin.ModelAdmin)
 admin.site.register(models.Device, admin.ModelAdmin)
