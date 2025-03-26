0d4c03e774bc83a50e2275f3f68479d8f917d415
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:40:14 +0000

    Moved patient and hospital patient to patient app
    (cherry picked from commit 02b69e92dc4c1d1422c16ec325baa23895b407d4)
    
    Co-authored-by: David Gagne <david.gagne@muhc.mcgill.ca>

--- opal/caregivers/api/serializer.py
+++ opal/caregivers/api/serializer.py
@@ -2,30 +2,14 @@
 from rest_framework import serializers
 
 from opal.caregivers.models import RegistrationCode
-from opal.patients.models import HospitalPatient, Patient
-
-
-class _PatientSerializer(serializers.ModelSerializer):
-    """Patient serializer for registration."""
-
-    class Meta:
-        model = Patient
-        fields = ['health_insurance_number']
-
-
-class _HospitalPatientSerializer(serializers.ModelSerializer):
-    """Hospital patient serializer for registration."""
-
-    class Meta:
-        model = HospitalPatient
-        fields = ['mrn', 'is_active']
+from opal.patients.serializer import HospitalPatientRegistrationSerializer, PatientRegistrationSerializer
 
 
 class RegistrationEncryptionInfoSerializer(serializers.ModelSerializer):
     """Serializer for the return value of registration encrytion info."""
 
-    patient = _PatientSerializer(source='relationship.patient', many=False, read_only=True)
-    hospital_patients = _HospitalPatientSerializer(
+    patient = PatientRegistrationSerializer(source='relationship.patient', many=False, read_only=True)
+    hospital_patients = HospitalPatientRegistrationSerializer(
         source='relationship.patient.hospital_patients',
         many=True,
         read_only=True,
