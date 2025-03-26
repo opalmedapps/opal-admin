1b11264ea43fe6c78fee09e8237df3a10596250e
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 00:38:51 +0000

    feat: remove/update old institution logo images
    (cherry picked from commit b1f6069bed9b0a81e922a01d143019655bab9264)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/hospital_settings/models.py
+++ opal/hospital_settings/models.py
@@ -1,7 +1,8 @@
 """This module provides models for hospital-specific settings."""
 
-from datetime import datetime as dt
+import os
 
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -30,13 +31,65 @@ class Institution(Location):
 
     logo = models.ImageField(
         _('Logo'),
-        upload_to='uploads/institution-logo/{0}/'.format(dt.now().strftime('%Y/%m/%d')),
+        upload_to='uploads/institution-logo/',
     )
 
     class Meta:
         ordering = ['name']
         verbose_name = _('Institution')
         verbose_name_plural = _('Institutions')
+
+
+    def save(self, *args, **kwargs) -> None:
+        _, f_en_extension = os.path.splitext(self.logo.name)
+        _, f_fr_extension = os.path.splitext(self.logo_fr.name)
+
+        try:
+            this = Institution.objects.get(id=self.id)
+            if this.logo != self.logo:
+                os.remove(this.logo.path)
+
+                self.logo.name = '{0}_{1}{2}'.format(self.code, 'logo_en', f_en_extension)
+
+            if this.logo_fr != self.logo_fr:
+                os.remove(this.logo_fr.path)
+
+                self.logo_fr.name = '{0}_{1}{2}'.format(self.code, 'logo_fr', f_fr_extension)
+
+            if this.code != self.code and this.logo == self.logo:
+                new_name = 'uploads/institution-logo/{0}_{1}{2}'.format(
+                    self.code,
+                    'logo_en',
+                    f_en_extension,
+                )
+                new_path = '{0}/{1}'.format(settings.MEDIA_ROOT, new_name)
+                os.rename(this.logo.path, new_path)
+                self.logo.name = new_name
+
+            if this.code != self.code and this.logo_fr == self.logo_fr:
+                new_name = 'uploads/institution-logo/{0}_{1}{2}'.format(
+                    self.code,
+                    'logo_fr',
+                    f_fr_extension,
+                )
+                new_path = '{0}/{1}'.format(settings.MEDIA_ROOT, new_name)
+                os.rename(this.logo_fr.path, new_path)
+                self.logo_fr.name = new_name
+        except Exception:
+            self.logo.name = '{0}_{1}{2}'.format(self.code, 'logo_en', f_en_extension)
+            self.logo_fr.name = '{0}_{1}{2}'.format(self.code, 'logo_fr', f_fr_extension)
+
+        super(Institution, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # check https://stackoverflow.com/questions/16041232/django-delete-filefield
+        # django cleanup
+        # your data might end up referencing a nonexistent file if your save() method call happens to be within a transaction that gets rolled back.
+        if os.path.isfile(self.logo.path):
+            os.remove(self.logo.path)
+        if os.path.isfile(self.logo_fr.path):
+            os.remove(self.logo_fr.path)
+        super(Institution, self).delete(*args, **kwargs)
 
 
 class Site(Location):
