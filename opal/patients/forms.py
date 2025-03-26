3178f1b617a1e184d8cb1d583b0980fda1796cc3
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 02:27:20 +0000

    fix: fix django stubs
    (cherry picked from commit 48bc05752f9147f4feac32643a692e4282f33757)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- opal/patients/forms.py
+++ opal/patients/forms.py
@@ -1010,7 +1010,7 @@ class ManageCaregiverAccessForm(forms.Form):
             site.initial = Site.objects.first()
 
 
-class ManageCaregiverAccessUserForm(forms.ModelForm[User]):
+class ManageCaregiverAccessUserForm(forms.ModelForm[Caregiver]):
     """Form for updating a `Caregiver` object."""
 
     class Meta:
