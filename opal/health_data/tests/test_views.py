dc881bf91ee95fd29d47ae116277fa06ef058a41
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-26 01:07:59 +0000

    Merge remote-tracking branch 'origin/main' into improve-renovate
    (cherry picked from commit 86186cb77d27cb2c3c629edcc10bf8ed11863f56)
    
    Co-authored-by: Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>

--- opal/health_data/tests/test_views.py
+++ opal/health_data/tests/test_views.py
@@ -8,16 +8,27 @@ import pytest
 from bs4 import BeautifulSoup
 from pytest_django.asserts import assertTemplateUsed
 
-from opal.patients import factories
+from opal.patients import factories as patient_factory
 from opal.users.models import User
 
+from .. import factories as healthdata_factory
+from ..models import QuantitySampleType
+
 pytestmark = pytest.mark.django_db
+
+MISSING_DATA_WARNINGS = (
+    'No data found for Body Mass',
+    'No data found for Body Temperature',
+    'No data found for Heart Rate',
+    'No data found for Heart Rate Variability',
+    'No data found for Oxygen Saturation',
+)
 
 
 def test_health_data_ui_template_used(user_client: Client, admin_user: AbstractUser) -> None:
     """Ensure the health data page can be rendered and patient info displayed."""
     user_client.force_login(admin_user)
-    hd_patient = factories.Patient(ramq='OTES12345678')
+    hd_patient = patient_factory.Patient(ramq='OTES12345678')
     response = user_client.get(reverse('health_data:health-data-ui', kwargs={'id': hd_patient.id}))
     soup = BeautifulSoup(response.content, 'html.parser')
     patient_identifiers = soup.find_all('h4')
@@ -34,7 +45,7 @@ def test_health_data_ui_unauthorized_no_data(user_client: Client) -> None:
     """Ensure that no health data appears if user missing permission."""
     unauthorized_user = User.objects.create(username='marge_simpson')
     user_client.force_login(unauthorized_user)
-    hd_patient = factories.Patient(ramq='OTES12345678')
+    hd_patient = patient_factory.Patient(ramq='OTES12345678')
     response = user_client.get(reverse('health_data:health-data-ui', kwargs={'id': hd_patient.id}))
 
     assert response.status_code == HTTPStatus.FORBIDDEN
@@ -45,3 +56,38 @@ def test_health_data_ui_error_no_patient(user_client: Client, admin_user: Abstra
     user_client.force_login(admin_user)
     response = user_client.get(reverse('health_data:health-data-ui', kwargs={'id': 42}))
     assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_health_data_template_plots_detected(user_client: Client, admin_user: AbstractUser) -> None:
+    """Ensure generate plotly content shows in template."""
+    user_client.force_login(admin_user)
+    patient = patient_factory.Patient()
+    healthdata_factory.QuantitySample(patient=patient, type=QuantitySampleType.BODY_MASS)
+    healthdata_factory.QuantitySample(patient=patient, type=QuantitySampleType.BODY_TEMPERATURE)
+    healthdata_factory.QuantitySample(patient=patient, type=QuantitySampleType.HEART_RATE)
+    healthdata_factory.QuantitySample(patient=patient, type=QuantitySampleType.HEART_RATE_VARIABILITY)
+    healthdata_factory.QuantitySample(patient=patient, type=QuantitySampleType.OXYGEN_SATURATION)
+
+    response = user_client.get(reverse('health_data:health-data-ui', kwargs={'id': patient.id}))
+    soup = BeautifulSoup(response.content, 'html.parser')
+    no_data_lines = soup.find_all('h5')
+    for line in no_data_lines:
+        if line.string:
+            assert line.string not in MISSING_DATA_WARNINGS
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_health_data_generate_plot_empty(user_client: Client, admin_user: AbstractUser) -> None:
+    """Ensure generate plot returns nothing given no QuantitySample data, and the correct info lines are shown."""
+    user_client.force_login(admin_user)
+    patient = patient_factory.Patient()
+
+    response = user_client.get(reverse('health_data:health-data-ui', kwargs={'id': patient.id}))
+    soup = BeautifulSoup(response.content, 'html.parser')
+    no_data_lines = soup.find_all('h5')
+    for line in no_data_lines:
+        if line.string:
+            assert line.string in MISSING_DATA_WARNINGS
+
+    assert response.status_code == HTTPStatus.OK
--- opal/health_data/views.py
+++ opal/health_data/views.py
@@ -1,12 +1,17 @@
 """This module provides views for any health-data related functionality."""
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext
 from django.views import generic
 
+import pandas as pd
+from plotly import express as px
+
 from ..patients.models import Patient
-from .models import QuantitySample
+from .models import QuantitySample, QuantitySampleType
 
 
 class HealthDataView(PermissionRequiredMixin, generic.TemplateView):
@@ -31,7 +36,69 @@ class HealthDataView(PermissionRequiredMixin, generic.TemplateView):
             Dict[str, Any]
         """
         context = super().get_context_data(**kwargs)
+        patient = get_object_or_404(Patient, id=self.kwargs['id'])
+
+        graphs = {}
+        for sample_type in QuantitySampleType:
+            graphs[sample_type.label.split(' (')[0]] = self._generate_plot(
+                title=str(sample_type.label),
+                label_x=gettext('Date'),
+                label_y=str(sample_type.label),
+                data=QuantitySample.objects.order_by('start_date').filter(
+                    patient=patient,
+                    type=sample_type,
+                ),
+            )
+
         context.update(
-            {'patient': get_object_or_404(Patient, id=self.kwargs['id'])},
+            {
+                'patient': patient,
+                'graphs': graphs,
+            },
         )
         return context
+
+    def _generate_plot(self, title: str, label_x: str, label_y: str, data: QuerySet[QuantitySample]) -> Optional[str]:
+        """Generate a plotly chart for the given sample type.
+
+        Args:
+            title: Plot title
+            label_x: x axis label
+            label_y: y axis label
+            data: QuantitySample queryset
+
+        Returns:
+            Html string representation of the plot
+        """
+        if data:
+            df = pd.DataFrame(data.values('start_date', 'value', 'device'))
+
+            #  Plotly chart customization: https://plotly.com/python/line-charts/
+            figure = px.line(
+                df,
+                x='start_date',
+                y='value',
+                title=title,
+                color='device',
+                markers=True,
+                labels={
+                    'start_date': label_x,
+                    'value': label_y,
+                    'device': gettext('Device'),
+                },
+                hover_data=['value', 'device'],
+            )
+
+            figure.update_layout({
+                'hovermode': 'x unified',
+                'dragmode': False,
+                'plot_bgcolor': '#ffffff',
+                'paper_bgcolor': '#ffffff',
+                'yaxis': {
+                    'gridcolor': '#f2f2f2',
+                    'rangemode': 'tozero',
+                },
+            })
+
+            return figure.to_html()  # type: ignore[no-any-return]
+        return None
