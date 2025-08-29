a3bfb3066d400f7592a55d720b882a3c026f9289
Zeyu Dou <dou3180@hotmail.com>
2025-08-29 15:27:30 -0400

    feat: add recover user account script (#1662)

--- opal/users/tests/test_commands.py
+++ opal/users/tests/test_commands.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+from pathlib import Path
 
 from django.core.management.base import CommandError
 
@@ -80,3 +81,44 @@ class TestAccountDeletion(CommandTestMixin):
         assert caregiver_models.CaregiverProfile.objects.count() == 0
         assert user_models.User.objects.count() == 0
         assert patient_models.Relationship.objects.count() == 0
+
+
+class TestAccountRecover(CommandTestMixin):
+    """Test class to group the `recover_account` command tests."""
+
+    def test_recover_missing_file_path(self) -> None:
+        """Ensure that the file_path argument is required."""
+        with pytest.raises(CommandError, match='the following arguments are required: file_path'):
+            self._call_command('recover_account')
+
+    def test_recover_user_with_success(self) -> None:
+        """Ensure the input user is recovered successfully using the backup data."""
+        patient = patient_factories.Patient.create()
+        caregiver = caregiver_factories.CaregiverProfile.create(user__email='test@test.com')
+        relationship = patient_factories.RelationshipType.create(role_type=patient_models.RoleType.SELF)
+        patient_factories.Relationship.create(patient=patient, caregiver=caregiver, type=relationship)
+        caregiver_factories.SecurityAnswer.create(user=caregiver)
+        caregiver_factories.Device.create(caregiver=caregiver)
+        patient_factories.HospitalPatient.create(patient=patient)
+
+        stdout, _stderr = self._call_command('delete_account', 'test@test.com')
+        # User was deleted
+        assert patient_models.Patient.objects.count() == 0
+        assert caregiver_models.CaregiverProfile.objects.count() == 0
+        assert user_models.User.objects.count() == 0
+        assert patient_models.Relationship.objects.count() == 0
+        assert caregiver_models.SecurityAnswer.objects.count() == 0
+        assert caregiver_models.Device.objects.count() == 0
+        assert patient_models.HospitalPatient.objects.count() == 0
+        with Path('test.json').open('w', encoding='utf-8') as file:
+            file.write(stdout)
+        stdout, _stderr = self._call_command('recover_account', 'test.json')
+        assert stdout == 'Data successfully recovered! Please delete the backup file.\n'
+        assert patient_models.Patient.objects.count() == 1
+        assert caregiver_models.CaregiverProfile.objects.count() == 1
+        assert user_models.User.objects.count() == 1
+        assert patient_models.Relationship.objects.count() == 1
+        assert caregiver_models.SecurityAnswer.objects.count() == 1
+        assert caregiver_models.Device.objects.count() == 1
+        assert patient_models.HospitalPatient.objects.count() == 1
+        Path.unlink(Path('test.json'))
