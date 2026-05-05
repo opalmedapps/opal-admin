e3ebffbd45990786643e38eb8f2e96ede18e2cec
Matthias Schoettle <git@mattsch.com>
2026-05-05 11:23:29 -0400

    chore(deps): downgrade authlib to v1.6.11 (#2050)

--- opal/services/fhir/tests/test_fhir.py
+++ opal/services/fhir/tests/test_fhir.py
@@ -68,7 +68,7 @@ class TestFHIRConnector:
 
     def test_init_invalid_private_key(self) -> None:
         """FHIRConnector initialization fails when an invalid private key is provided."""
-        with pytest.raises(ValueError, match='Could not deserialize key data\\. '):
+        with pytest.raises(ValueError, match='Unable to load PEM file'):
             FHIRConnector(
                 oauth_url='https://example.com/oauth',
                 fhir_url='https://example.com/fhir',
